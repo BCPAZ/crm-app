b5070bff48ae29e32e53c0a29906c08ef0253c31
farildev <memmedovfaril@gmail.com>
2024-08-05 13:33:54 +0400

    feat : Navigation component created & add to appLayout

---
+++ src/components/App/Navigation.jsx
@@ -0,0 +1,9 @@
+const Navigation = () => {
+  return (
+    <nav className="flex items-center justify-center">
+      <div className="max-w-1030px w-full"></div>
+    </nav>
+  )
+}
+
+export default Navigation
--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,4 +1,5 @@
 import Header from "@/components/App/Header";
+import Navigation from "@/components/App/Navigation";
 import { Fragment } from "react";
 import { Outlet } from "react-router-dom";
 
@@ -6,6 +7,7 @@ const AppLayout = () => {
   return (
     <Fragment>
       <Header />
+      <Navigation />
       <main>
         <Outlet />
       </main>
