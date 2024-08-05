33ec347e7e7ca38942cb3f62069ad710c4ccd612
farildev <memmedovfaril@gmail.com>
2024-08-05 12:43:58 +0400

    feat : Proptypes added to components

--- src/components/Auth/AuthButton.jsx
+++ src/components/Auth/AuthButton.jsx
@@ -1,7 +1,13 @@
+import PropTypes from "prop-types";
+
 const AuthButton = ({value}) => {
   return (
     <button className="bg-secondary w-full text-white p-3 rounded-lg font-bold mb-5">{value}</button>
   )
+}
+
+AuthButton.propTypes = {
+  value : PropTypes.string.isRequired
 }
 
 export default AuthButton
--- src/components/Auth/AuthInput.jsx
+++ src/components/Auth/AuthInput.jsx
@@ -1,24 +1,36 @@
-import { FaEyeSlash,FaEye } from "react-icons/fa"
-const AuthInput = ({type , placeholder, label,}) => {
+import { FaEyeSlash } from "react-icons/fa";
+import PropTypes from "prop-types";
+
+const AuthInput = ({ type, placeholder, label }) => {
   return (
     <div className="flex flex-col relative border">
-            <label
-              className="text-xs font-semibold text-gray-500 absolute bg-white -top-3 p-1 left-4 w-fit"
-              htmlFor="#password"
-            >
-              {label}
-            </label>
-            <div className="w-full flex items-center justify-between p-4 rounded-lg text-sm bg-gray-300/20">
-              <input
-                id="password"
-                className="w-full outline-none h-full bg-transparent"
-                type={type}
-                placeholder={placeholder}
-              />
-              {type === 'password' && <button type="button"><FaEyeSlash size={20}/></button>}
-            </div>
-          </div>
-  )
+      <label
+        className="text-xs font-semibold text-gray-500 absolute bg-white -top-3 p-1 left-4 w-fit"
+        htmlFor="#password"
+      >
+        {label}
+      </label>
+      <div className="w-full flex items-center justify-between p-4 rounded-lg text-sm bg-gray-300/20">
+        <input
+          id="password"
+          className="w-full outline-none h-full bg-transparent"
+          type={type}
+          placeholder={placeholder}
+        />
+        {type === "password" && (
+          <button type="button">
+            <FaEyeSlash size={20} />
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+AuthInput.propTypes = {
+  type : PropTypes.string.isRequired,
+  placeholder : PropTypes.string.isRequired,
+  label : PropTypes.string.isRequired
 }
 
-export default AuthInput
+export default AuthInput;
