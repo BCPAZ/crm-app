82033a193b1b43fa7fa7d3d245af3cef44c11434
farildev <memmedovfaril@gmail.com>
2024-08-05 13:30:01 +0400

    feat : Logo and buttons added & header completed

--- src/components/App/Header.jsx
+++ src/components/App/Header.jsx
@@ -1,6 +1,26 @@
+import notifications from "@/assets/icons/notifications.svg";
+import english from "@/assets/icons/english.svg";
+import avatar from "@/assets/images/avatar.jpg";
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="h-16 flex items-center">
+      <div className="siteContainer flex items-center justify-between">
+        <div className="logo">
+          <img className="w-[50px]" src="https://cdn-icons-png.flaticon.com/512/906/906341.png" alt="" />
+        </div>
+        <div className="flex items-center gap-2">
+          <button type="button">
+            <img src={english} alt="Language changed" />
+          </button>
+          <button type="button">
+            <img src={notifications} alt="Language changed" />
+          </button>
+          <button className="w-[40px] h-[40px] overflow-hidden p-1 rounded-full border-2 border-gray-300/20">
+            <img className="w-full h-full object-cover object-center" src={avatar} alt="" />
+          </button>
+        </div>
+      </div>
+    </header>
   )
 }
 
--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,11 +1,16 @@
-import { Outlet } from 'react-router-dom'
+import Header from "@/components/App/Header";
+import { Fragment } from "react";
+import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
   return (
-    <main>
-      <Outlet />
-    </main>
-  )
-}
+    <Fragment>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+    </Fragment>
+  );
+};
 
-export default AppLayout
+export default AppLayout;
