b561445cf2b0d27f2fca90869ab699574c477747
farildev <memmedovfaril@gmail.com>
2024-08-05 13:14:09 +0400

    feat : Project pages added to route

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,14 +4,15 @@ import AppLayout from "@/layouts/AppLayout";
 import Login from "@/views/Auth/Login";
 import ResetPassword from "@/views/Auth/ResetPassword";
 import ForgotPassword from "@/views/Auth/ForgotPassword";
+import Projects from "@/views/App/Projects";
 const AppRouter = () => {
   return (
     <Routes>
       <Route element={<AppLayout />}>
-
+        <Route path="/" element={<Projects />} />
       </Route>
       <Route element={<AuthLayout />}>
-          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/reset-password" element={<ResetPassword />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
       </Route>
