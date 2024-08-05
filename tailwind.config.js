32ed2f9978d07ad848bc9fa3376a69704f4291f0
farildev <memmedovfaril@gmail.com>
2024-08-05 11:01:37 +0400

    feat : Tailwind & reset css added to project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily : {
+      primary : 'Plus Jakarta Sans'
+    },
+    extend: {
+      colors : {
+        secondary : '#003458',
+        light : '#fff'
+      }
+    },
+  },
+  plugins: [],
+}
