394dc58d683f9a991c17f065d58a93c34526cc51
farildev <memmedovfaril@gmail.com>
2024-08-06 16:31:02 +0400

    feat : Upload new document page heading style completed

--- src/views/App/Documents/UploadNewDocument.jsx
+++ src/views/App/Documents/UploadNewDocument.jsx
@@ -1,7 +1,27 @@
-  const UploadNewDocument = () => {
+const UploadNewDocument = () => {
   return (
-    <div>UploadNewDocument</div>
-  )
-}
+    <section>
+      <div className="border-b border-gray-400 py-7">
+        <div className="siteContainer">
+          <h1 className="text-xl font-semibold">Upload document</h1>
+        </div>
+      </div>
+      <div className="max-w-[900px] w-full flex justify-center mx-auto px-5 gap-5 py-10">
+        <div className="w-1/2">
+          salam
+        </div>
+        <div className="w-1/2">
+          <p className="w-full">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis
+            reprehenderit sed quibusdam at aliquam, dolore fugiat quidem aut
+            vitae accusantium vero, incidunt minus molestiae dolorum. Lorem
+            ipsum dolor sit amet consectetur adipisicing elit. Nam ab qui
+            possimus enim, nostrum culpa eveniet autem eum ipsa adipisci?
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default UploadNewDocument
+export default UploadNewDocument;
