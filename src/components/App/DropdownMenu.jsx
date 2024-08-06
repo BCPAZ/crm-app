861db4a86d8fb4a5de9f1afdec9e4ebe1cd4884e
farildev <memmedovfaril@gmail.com>
2024-08-06 11:00:06 +0400

    feat : Navigation dropdown completed

---
+++ src/components/App/DropdownMenu.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+import { LuChevronDown } from "react-icons/lu";
+
+const DropdownMenu = ({ navElement }) => {
+  return (
+    <div className="relative">
+      <div
+        className={
+          "flex items-center gap-2 text-white text-sm font-semibold hover:bg-white/30 p-2 rounded-md group"
+        }
+      >
+        <img src={navElement.icon} alt="" />
+        <span>{navElement.title}</span>
+        <button>
+          <LuChevronDown />
+        </button>
+      </div>
+      <ul className="w-[254px] rounded-lg outline-none border-none absolute top-full left-0 shadow-lg bg-white p-2 overflow-hidden hidden">
+        <div className="bg-green-900 h-[100px] w-[100px] rounded-full blur-[50px] absolute -top-[25%] -right-[10%]"></div>
+        {navElement.elements.length > 0
+          ? navElement.elements.map((el, index) => (
+              <li key={index}>
+                <Link to={el.path} className="block p-2 text-gray-500 hover:bg-gray-200 rounded-md text-sm">
+                  {el.label}
+                </Link>
+              </li>
+            ))
+          : null}
+      </ul>
+    </div>
+  );
+};
+
+export default DropdownMenu;
--- src/components/App/Navigation.jsx
+++ src/components/App/Navigation.jsx
@@ -1,13 +1,11 @@
 import { navigationLinks } from "@/utils/constants";
-import { Link } from "react-router-dom";
+import DropdownMenu from "./DropdownMenu";
 const Navigation = () => {
   return (
     <nav className="flex items-center justify-center bg-secondary h-[75px] w-full p-5">
       <div className="max-w-[1030px] w-full flex items-center gap-2 justify-between">
         {navigationLinks.map((nav, index) => (
-          <Link key={index} to={nav.path}>
-            
-          </Link>
+            <DropdownMenu navElement={nav} key={index} />
         ))}
       </div>
     </nav>
