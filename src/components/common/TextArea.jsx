71e65d9d183182d6e6bb169464bbac6427f3df66
farildev <memmedovfaril@gmail.com>
2024-08-06 15:03:05 +0400

    feat : textarea component created

---
+++ src/components/common/TextArea.jsx
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types";
+const TextArea = ({placeholder, label}) => {
+  return (
+    <div className="flex flex-col relative w-full">
+      <label
+        className="text-xs font-semibold text-gray-500 absolute bg-white -top-3 p-1 left-4 w-fit"
+      >
+        {label}
+      </label>
+      <textarea className="w-full resize-none p-4 rounded-lg text-sm border border-grey/20" placeholder={placeholder}></textarea>
+    </div>
+  )
+}
+
+TextArea.propTypes = {
+  placeholder : PropTypes.string.isRequired,
+  label : PropTypes.string.isRequired
+}
+
+export default TextArea
