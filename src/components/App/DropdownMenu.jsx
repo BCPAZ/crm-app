84216d86060cd7925b9a2b7b874a77fde6ce98b9
farildev <memmedovfaril@gmail.com>
2024-08-06 13:43:52 +0400

    fix : dropdown hover bug fixed

--- src/components/App/DropdownMenu.jsx
+++ src/components/App/DropdownMenu.jsx
@@ -4,42 +4,48 @@ import PropTypes from "prop-types";
 
 const DropdownMenu = ({ navElement }) => {
   return (
-    <div className="relative">
-      <div
-        className={
-          "flex items-center gap-2 text-white text-sm font-semibold hover:bg-white/10 p-2 rounded-md group"
-        }
+    <div className="relative group">
+      <button
+        className="flex items-center gap-2 text-white text-sm font-semibold hover:bg-white/10 p-2 rounded-md"
       >
         <img src={navElement.icon} alt="" />
         <span>{navElement.title}</span>
-        {
-          navElement.elements && <button className="outline-none">
-          <LuChevronDown />
-        </button>
-        }
-      </div>
-      <ul className="w-[254px] rounded-lg outline-none border-none absolute top-full left-0 shadow-lg bg-white p-2 overflow-hidden hidden">
-        <div className="bg-green-900 h-[100px] w-[100px] rounded-full blur-[50px] absolute -top-[25%] -right-[10%]"></div>
-        {navElement.elements
-          ? navElement.elements.map((el, index) => (
-              <li key={index}>
-                <Link to={el.path} className="block p-2 text-gray-500 hover:bg-gray-200 rounded-md text-sm">
-                  {el.label}
-                </Link>
-              </li>
-            ))
-          : null}
-      </ul>
+        {navElement.elements && (
+          <button className="outline-none">
+            <LuChevronDown />
+          </button>
+        )}
+      </button>
+      {navElement.elements && (
+        <ul className="w-[254px] group-hover:block hidden rounded-lg outline-none border-none absolute top-[107%] left-0 shadow-lg bg-white p-2 overflow-hidden">
+          <div className="bg-green-900 h-[100px] w-[100px] rounded-full blur-[50px] absolute -top-[25%] -right-[10%]"></div>
+          {navElement.elements.map((el, index) => (
+            <li key={index}>
+              <Link
+                to={el.path}
+                className="block p-2 text-gray-500 hover:bg-gray-200 rounded-md text-sm"
+              >
+                {el.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
 
 DropdownMenu.propTypes = {
-  navElement : PropTypes.shape({
-    icon : PropTypes.string.isRequired,
-    title : PropTypes.string.isRequired,
-    elements : PropTypes.array
-  })
-}
+  navElement: PropTypes.shape({
+    icon: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    elements: PropTypes.arrayOf(
+      PropTypes.shape({
+        path: PropTypes.string.isRequired,
+        label: PropTypes.string.isRequired,
+      })
+    ),
+  }).isRequired,
+};
 
 export default DropdownMenu;
