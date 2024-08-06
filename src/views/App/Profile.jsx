e68149375e83588c77e8c1c52bfa1b4bff038b48
farildev <memmedovfaril@gmail.com>
2024-08-06 15:37:09 +0400

    feat : Profile page completed

--- src/views/App/Profile.jsx
+++ src/views/App/Profile.jsx
@@ -1,12 +1,24 @@
 import Input from "@/components/common/Input";
 import TextArea from "@/components/common/TextArea";
+import { IoCamera } from "react-icons/io5";
+
 const Profile = () => {
   return (
-    <section className="h-screen">
+    <section className="">
       <div className="siteContainer">
         <h1 className="text-2xl mt-10 font-semibold">Profile</h1>
-        <div className="w-full flex lg:flex-row flex-col justify-between gap-10" action="">
-          <div className="lg:w-[38%] w-full">Lala</div>
+        <div className="w-full flex lg:flex-row flex-col justify-between gap-10 mt-24" action="">
+          <div className="lg:w-[38%] w-full flex items-center flex-col">
+            <div className="w-40 h-40 rounded-full overflow-hidden border-2 border-gray-500/50 relative group">
+              <img className="w-full h-full object-cover" src="https://sm.ign.com/ign_nordic/cover/a/avatar-gen/avatar-generations_prsz.jpg" alt="" />
+              <div className="hidden group-hover:flex flex-col gap-2 items-center justify-center w-full h-full bg-black/50 absolute top-0 left-0 right-0 bottom-0">
+                <IoCamera size={24} color="white"/>
+                <span className="text-sm text-white">Update photo</span>
+              </div>
+            </div>
+            <p className="text-xs text-gray-400 max-w-[174px] text-center mt-6">Allowed *.jpeg, *.jpg, *.png, *.gif
+            Max size of 3.1 MB</p>
+          </div>
           <div className="lg:w-[62%] w-full">
             <div className="grid md:grid-cols-2 grid-cols-1 gap-6">
               <Input label="Name" placeholder="Enter your name" type="text" />
