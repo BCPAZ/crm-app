17a9288e348ddc22203c527af56eaef94c622357
farildev <memmedovfaril@gmail.com>
2024-08-07 23:32:41 +0400

    feat : Create template header added to page

--- src/views/App/Workflow/CreateTemplate.jsx
+++ src/views/App/Workflow/CreateTemplate.jsx
@@ -1,7 +1,22 @@
+import Button from "@/components/common/Button";
+
 const CreateTemplate = () => {
   return (
-    <div>CreateTemplate</div>
-  )
-}
+    <section>
+      <div className="border-b border-gray-400 py-7">
+        <div className="siteContainer">
+          <div className="flex items-center justify-between">
+            <h1 className="text-xl font-semibold w-full">
+              Create Template - Workflow
+            </h1>
+            <div className="w-[15%]">
+              <Button value="Save" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default CreateTemplate
+export default CreateTemplate;
