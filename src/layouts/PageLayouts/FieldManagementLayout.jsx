a63485f2e6bd1ac6052e563fb7b7f8cf3a4b31d7
farildev <memmedovfaril@gmail.com>
2024-08-08 14:03:11 +0400

    feat : Field management layout changed

--- src/layouts/PageLayouts/FieldManagementLayout.jsx
+++ src/layouts/PageLayouts/FieldManagementLayout.jsx
@@ -1,11 +1,26 @@
-import { Outlet } from "react-router-dom"
+import Sidebar from "@/components/App/Documents/Sidebar";
+import { Outlet } from "react-router-dom";
 
 const FieldManagementLayout = () => {
   return (
-    <main>
-      <Outlet />
-    </main>
-  )
-}
+    <div className="h-screen">
+      <div className="border-b border-gray-400 py-7">
+        <div className="siteContainer">
+          <div className="flex items-center justify-between">
+            <h1 className="text-xl font-semibold w-full">Project Settings</h1>
+          </div>
+        </div>
+      </div>
+      <div className="w-full flex justify-between relative">
+        <div className="w-[25%] absolute top-0 left-0">
+          <Sidebar />
+        </div>
+        <main className="w-[75%] absolute top-0 right-0">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+};
 
-export default FieldManagementLayout
+export default FieldManagementLayout;
--- src/views/App/FieldManagement/ProjectSettings.jsx
+++ src/views/App/FieldManagement/ProjectSettings.jsx
@@ -1,6 +1,10 @@
 const ProjectSettings = () => {
   return (
-    <div>ProjectSettings</div>
+    <section className="px-[60px] py-6">
+      <div className="siteContainer">
+        <h1 className="text-xl font-semibold">Names and designations</h1>
+      </div>
+    </section>
   )
 }
 
