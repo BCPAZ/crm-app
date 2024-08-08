52856199a7250725ecdd10df8bace7156f50f6bc
farildev <memmedovfaril@gmail.com>
2024-08-08 14:47:58 +0400

    feat : Filter forms completed in create template

--- src/components/common/SecondTextArea.jsx
+++ src/components/common/SecondTextArea.jsx
@@ -1,17 +1,19 @@
 import PropTypes from "prop-types";
 
-const SecondTextArea = ({placeholder, label}) => {
+const SecondTextArea = ({placeholder, label, column, solid}) => {
   return (
-    <div className="flex flex-col gap-2 w-full">
-      <label className="text-md text-gray-500 font-light">{label}</label>
-      <textarea className="w-full outline-none min-h-24 resize-none p-4 rounded-lg text-sm border border-grey/20" placeholder={placeholder}/>
+    <div className={`flex ${column ? 'flex-col' : 'flex-row items-center'} gap-2 w-full`}>
+      <label className="text-sm text-gray-500 font-light">{label}</label>
+      <textarea className={`w-full outline-none min-h-24 resize-none p-4 rounded-lg text-sm ${solid ? 'bg-grey/20' : 'border border-grey/20'}`} placeholder={placeholder}/>
     </div>
   )
 }
 
 SecondTextArea.propTypes = {
   placeholder : PropTypes.string.isRequired,
-  label : PropTypes.string.isRequired
+  label : PropTypes.string.isRequired,
+  column : PropTypes.bool,
+  solid : PropTypes.bool
 }
 
 export default SecondTextArea
--- src/views/App/Workflow/CreateTemplate.jsx
+++ src/views/App/Workflow/CreateTemplate.jsx
@@ -1,4 +1,8 @@
 import Button from "@/components/common/Button";
+import CheckboxElement from "@/components/common/CheckboxElement";
+import SecondInput from "@/components/common/SecondInput";
+import SecondTextArea from "@/components/common/SecondTextArea";
+import Select from "@/components/common/Select";
 
 const CreateTemplate = () => {
   return (
@@ -12,6 +16,48 @@ const CreateTemplate = () => {
             <div className="w-[15%]">
               <Button value="Save" />
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="siteContainer">
+        <div className="py-8 md:max-w-[750px] max-w-full">
+          <div className="flex justify-center flex-col gap-5">
+            <SecondInput column label="Name *" placeholder="Template Name" />
+            <SecondTextArea
+              solid
+              column
+              label="Description"
+              placeholder="Description"
+            />
+            <Select column label="Approval statuses" />
+          </div>
+        </div>
+        <div className="grid md:grid-cols-2 grid-cols-1 gap-[60px] mt-10">
+          <div className="flex md:flex-row flex-col gap-10">
+            <h3 className="md:max-w-[82px] max-w-full font-medium">Initiators capabilities</h3>
+            <div className="flex flex-col gap-4">
+              <div className="flex flex-col gap-3">
+                <h3>Beginning of the process</h3>
+                <CheckboxElement label="The initiator can change the participants and the duration of the stages" />
+              </div>
+              <div className="flex flex-col gap-3">
+                <h3>During the process execution</h3>
+                <CheckboxElement label="The initiator can change the list of participants in the stage" />
+                <CheckboxElement label="The initiator can skip stages" />
+              </div>
+            </div>
+          </div>
+          <div className="flex md:flex-row flex-col md:gap-10 gap-5">
+            <h3 className="md:max-w-[82px] max-w-full">Rules for completing stages</h3>
+            <div className="flex flex-col gap-4 w-full">
+              <Select column label="Parallel stages are completed if"/>
+              <Select column label="If rejected"/>
+            </div>
+          </div>
+          <div className="flex md:flex-row flex-col md:gap-10 gap-5">
+            <h3 className="w-fit">Attributes of 
+            writing</h3>
+            <Select />
           </div>
         </div>
       </div>
