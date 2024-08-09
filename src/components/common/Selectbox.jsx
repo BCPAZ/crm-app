d56a1cde4d91a97e880bd555818fbdc927e66d4e
farildev <memmedovfaril@gmail.com>
2024-08-09 10:08:33 +0400

    fix : style collision fixed

--- src/components/common/Selectbox.jsx
+++ src/components/common/Selectbox.jsx
@@ -10,12 +10,12 @@ export default function Selectbox({outline}) {
         <div className="relative">
           <Select
             className={clsx(
-              `mt-3 block w-full appearance-none rounded-lg border-none ${outline ? 'text-black border border-grey/20' : 'bg-secondary text-white'} py-1.5 px-3 text-sm`,
+              `mt-3 block w-full appearance-none rounded-lg ${outline ? 'text-black border border-grey/20' : 'bg-secondary text-white'} py-1.5 px-3 text-sm`,
               'focus:outline-none data-[focus]:-outline-offset-2',
               '*:text-black'
             )}
           >
-            <option value="active">Active</option>
+            <option value="active">AZINTEST</option>
             <option value="paused">Paused</option>
             <option value="delayed">Delayed</option>
             <option value="canceled">Canceled</option>
--- src/views/App/Insights.jsx
+++ src/views/App/Insights.jsx
@@ -1,12 +1,17 @@
+import Selectbox from "@/components/common/Selectbox"
+
 const Insights = () => {
   return (
     <section>
       <div className="border-b border-gray-400 py-7">
         <div className="siteContainer">
-          <div className="flex items-center justify-between">
+          <div className="flex items-center gap-3">
             <h1 className="text-xl font-semibold w-full">
               Projects Overview
             </h1>
+            <Selectbox  />
+            <Selectbox outline />
+            <Selectbox outline />
           </div>
         </div>
       </div>
