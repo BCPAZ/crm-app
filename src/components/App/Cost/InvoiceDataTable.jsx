5c8f5161116b57b9497454f9dd2b9f8d4901f4f9
farildev <memmedovfaril@gmail.com>
2024-08-10 11:17:15 +0400

    feat : Invoice user data's added to table

--- src/components/App/Cost/InvoiceDataTable.jsx
+++ src/components/App/Cost/InvoiceDataTable.jsx
@@ -1,6 +1,9 @@
 import CheckboxElement from "@/components/common/CheckboxElement";
-import avatar from "@/assets/images/img.png";
+import userInvoices from "@/mocks/userInvoices";
 import { GoArrowDown } from "react-icons/go";
+import { Link } from "react-router-dom";
+import { LuMoreVertical } from "react-icons/lu";
+
 
 const InvoiceDataTable = () => {
   return (
@@ -8,76 +11,90 @@ const InvoiceDataTable = () => {
       <table className="w-full text-sm min-w-[1200px]">
         <thead className="bg-gray-300/30 w-full rounded-lg text-left">
           <tr className="p-5 w-full flex items-center justify-between gap-5">
-            <th className="text-sm font-medium text-gray-500 flex items-center gap-3 rounded-s-lg w-[50%]">
+            <th className="text-sm font-medium text-gray-500 flex items-center gap-3 rounded-s-lg w-[40%]">
               <CheckboxElement />
-              <span className="flex items-center gap-2">No Document <GoArrowDown /></span>
+              <span className="flex items-center gap-2">
+                User <GoArrowDown />
+              </span>
             </th>
-            <th className="text-sm font-medium w-[10%] text-gray-500">Stage</th>
-            <th className="text-sm font-medium w-[10%] text-gray-500">Results</th>
-            <th className="text-sm font-medium w-[10%] text-gray-500">Deadline</th>
-            <th className="text-sm font-medium w-[10%] text-gray-500 text-right">Executor</th>
-            <th className="text-sm font-medium w-[10%] text-gray-500 rounded-e-lg">Status results</th>
+            <th className="text-sm font-medium w-[11%] text-gray-500">
+              Create
+            </th>
+            <th className="text-sm font-medium w-[11%] text-gray-500">Due</th>
+            <th className="text-sm font-medium w-[11%] text-gray-500">
+              Amount
+            </th>
+            <th className="text-sm font-medium w-[11%] text-gray-500">Sent</th>
+            <th className="text-sm font-medium w-[11%] text-gray-500 rounded-e-lg">
+              Status
+            </th>
+            <th className="text-sm font-medium w-[5%] text-gray-500 rounded-e-lg">
+            </th>
           </tr>
         </thead>
-        <tbody className="w-full flex flex-col text-left mt-5">
-          <tr className="p-5 border-b border-grey/20  w-full flex items-center justify-between gap-5 min-h-[76px]">
-            <th className="text-sm font-medium text-gray-500 flex items-center gap-3 rounded-s-lg w-[50%]">
-              <CheckboxElement />
-              <span>fraction-health-sao-tome-and-principe-helen-reddy-045706-sm-151104-ulan-bator.jpg</span>
-            </th>
-            <td className="text-sm font-medium text-gray-500 w-[10%]">Structure 1</td>
-            <td className="text-sm font-medium text-gray-500 w-[10%]">Rejected</td>
-            <td className="text-sm font-medium text-gray-500 w-[10%] flex flex-col gap-1">
-              <span className="text-xs">09 Aug 2022</span>
-              <span className="text-xs">10:37 AM</span>
-            </td>
-            <td className="text-sm font-medium text-gray-500 w-[10%] text-right flex items-center justify-end">
-              <img src={avatar} alt="" />
-              <img src={avatar} alt="" />
-              <img src={avatar} alt="" />
-            </td>
-            <td className="text-sm font-medium text-gray-500 w-[10%] rounded-e-lg">Approved</td>
-          </tr>
-          <tr className="p-5 border-b border-grey/20 w-full flex items-center justify-between gap-5 min-h-[76px]">
-            <th className="text-sm font-medium text-gray-500 flex items-center gap-3 rounded-s-lg w-[50%]">
-              <CheckboxElement />
-              <span>fraction-health-sao-tome-and-principe-helen-reddy-045706-sm-151104-ulan-bator.jpg</span>
-            </th>
-            <td className="text-sm font-medium text-gray-500 w-[10%]">Structure 1</td>
-            <td className="text-sm font-medium text-gray-500 w-[10%]">Rejected</td>
-            <td className="text-sm font-medium text-gray-500 w-[10%] flex flex-col gap-1">
-              <span className="text-xs">09 Aug 2022</span>
-              <span className="text-xs">10:37 AM</span>
-            </td>
-            <td className="text-sm font-medium text-gray-500 w-[10%] text-right flex items-center justify-end">
-              <img src={avatar} alt="" />
-              <img src={avatar} alt="" />
-              <img src={avatar} alt="" />
-            </td>
-            <td className="text-sm font-medium text-gray-500 w-[10%] rounded-e-lg">Approved</td>
-          </tr>
-          <tr className="p-5 border-b border-grey/20 w-full flex items-center justify-between gap-5 min-h-[76px]">
-            <th className="text-sm font-medium text-gray-500 flex items-center gap-3 rounded-s-lg w-[50%]">
-              <CheckboxElement />
-              <span>fraction-health-sao-tome-and-principe-helen-reddy-045706-sm-151104-ulan-bator.jpg</span>
-            </th>
-            <td className="text-sm font-medium text-gray-500 w-[10%]">Structure 1</td>
-            <td className="text-sm font-medium text-gray-500 w-[10%]">Rejected</td>
-            <td className="text-sm font-medium text-gray-500 w-[10%] flex flex-col gap-1">
-              <span className="text-xs">09 Aug 2022</span>
-              <span className="text-xs">10:37 AM</span>
-            </td>
-            <td className="text-sm font-medium text-gray-500 w-[10%] text-right flex items-center justify-end">
-              <img src={avatar} alt="" />
-              <img src={avatar} alt="" />
-              <img src={avatar} alt="" />
-            </td>
-            <td className="text-sm font-medium text-gray-500 w-[10%] rounded-e-lg">Approved</td>
-          </tr>
+        <tbody className="w-full flex flex-col text-left">
+          {userInvoices.map((invoice, index) => (
+            <Link className="group" to={`${invoice.id}`} key={index}>
+              <tr className="p-5 border-b group-hover:bg-gray-200/20 border-grey/20  w-full flex items-center justify-between gap-5 min-h-[76px]">
+                <th className="text-sm font-medium text-gray-500 flex items-center gap-3 rounded-s-lg w-[40%]">
+                  <CheckboxElement />
+                  <div className="flex items-center gap-4">
+                    <img
+                      className="w-[40px] h-[40px] rounded-full"
+                      src={invoice.avatar}
+                      alt=""
+                    />
+                    <div className="flex flex-col">
+                      <h3 className="text-sm text-secondary">{invoice.name}</h3>
+                      <span className="text-sm text-gray-400">
+                        {invoice.code}
+                      </span>
+                    </div>
+                  </div>
+                </th>
+                <td className="text-sm font-medium text-gray-500 w-[11%]">
+                  <div className="flex flex-col">
+                    <h3 className="text-xs text-secondary">
+                      {invoice.createDate}
+                    </h3>
+                    <span className="text-xs text-gray-400">
+                      {invoice.createTime}
+                    </span>
+                  </div>
+                </td>
+                <td className="text-sm font-medium text-gray-500 w-[11%]">
+                  <div className="flex flex-col">
+                    <h3 className="text-xs text-secondary">
+                      {invoice.dueDate}
+                    </h3>
+                    <span className="text-xs text-gray-400">
+                      {invoice.dueTime}
+                    </span>
+                  </div>
+                </td>
+                <td className="text-sm font-medium text-gray-500 w-[11%]">
+                  <span className="text-sm text-secondary">
+                    ${invoice.amount}
+                  </span>
+                </td>
+                <td className="text-sm font-medium text-gray-500 w-[11%]">
+                  <span className="text-sm text-secondary">
+                    ${invoice.sent}
+                  </span>
+                </td>
+                <td className="text-sm font-medium text-gray-500 w-[11%] rounded-e-lg">
+                  <span className="text-sm capitalize">{invoice.status}</span>
+                </td>
+                <td className="text-sm font-medium text-gray-500 w-[5%] rounded-e-lg flex items-center justify-center">
+                  <button className="outline-none border-none" type="button"><LuMoreVertical size={18}/></button>
+                </td>
+              </tr>
+            </Link>
+          ))}
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default InvoiceDataTable
+export default InvoiceDataTable;
