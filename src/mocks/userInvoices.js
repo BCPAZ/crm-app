35e9b9e5c4e45cf4cbbe67c886135b8c0a82638c
farildev <memmedovfaril@gmail.com>
2024-08-10 10:56:06 +0400

    chore : userInvoices mock data created

---
+++ src/mocks/userInvoices.js
@@ -0,0 +1,40 @@
+const userInvoices = [
+  {
+    avatar : 'https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671122.jpg',
+    name : 'Jayvion Simon',
+    code : 'INV-1708',
+    createDate : '12 Jan 2024',
+    createTime : '10:00 PM',
+    dueDate : '11 Feb 2024',
+    dueTime : '9:00 PM',
+    amount : 16.19,
+    sent : 52,
+    status : 'pending'
+  },
+  {
+    avatar : 'https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671142.jpg',
+    name : 'George Rodrigues',
+    code : 'INV-1823',
+    createDate : '19 Jan 2024',
+    createTime : '14:00 PM',
+    dueDate : '24 Feb 2024',
+    dueTime : '9:00 PM',
+    amount : 32.45,
+    sent : 66,
+    status : 'paid'
+  },
+  {
+    avatar : 'https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671118.jpg',
+    name : 'Deja Brady',
+    code : 'INV-1719',
+    createDate : '24 Mar 2024',
+    createTime : '14:00 PM',
+    dueDate : '5 Jun 2024',
+    dueTime : '9:00 PM',
+    amount : 44.55,
+    sent : 27,
+    status : 'denied'
+  },
+]
+
+export default userInvoices
