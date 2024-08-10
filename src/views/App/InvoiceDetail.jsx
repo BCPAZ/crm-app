f62eab44daea8e194e11e16093d52c18f907ecaa
farildev <memmedovfaril@gmail.com>
2024-08-10 12:52:21 +0400

    feat : Invoice detail card created

--- src/views/App/InvoiceDetail.jsx
+++ src/views/App/InvoiceDetail.jsx
@@ -4,12 +4,26 @@ import userInvoices from "@/mocks/userInvoices";
 import Breadcrumb from "@/components/App/Cost/Breadcrumb";
 import NotFound from "./NotFound";
 import { invoiceIcons } from "@/utils/constants";
+import Select from "@/components/common/Select";
 const InvoiceDetail = () => {
   const { id } = useParams();
   const data = userInvoices.find((invoice) => invoice.id === parseInt(id));
 
   if (!data) {
     return <NotFound name="Invoice" />;
+  }
+
+  const renderStatus = (status) => {
+    switch(status) {
+      case 'denied':
+        return <span className="text-xs py-1 px-2  rounded bg-red-600/20 text-red-600 capitalize w-full">{status}</span>;
+      case 'paid':
+        return <span className="text-xs py-1 px-2  rounded bg-green-600/20 text-green-600 capitalize w-full">{status}</span>;
+      case 'pending':
+        return <span className="text-xs py-1 px-2 rounded bg-yellow-600/20 text-yellow-600 capitalize w-full">{status}</span>;
+      default:
+        return <span className="text-xs py-1 px-2  rounded bg-gray-600/40 capitalize w-full">{status}</span>;
+    }
   }
   return (
     <sections>
@@ -28,7 +42,7 @@ const InvoiceDetail = () => {
           <h1 className="text-2xl font-semibold">{data.code}</h1>
           <Breadcrumb />
         </div>
-        <div>
+        <div className="flex items-center justify-between">
           <div className="flex items-center gap-2">
             {invoiceIcons.map((invIcon, index) => (
               <button
@@ -42,6 +56,20 @@ const InvoiceDetail = () => {
               </button>
             ))}
           </div>
+          <div className="w-fit">
+          <Select />
+          </div>
+        </div>
+        <div className="mt-10 bg-white rounded-lg shadow-lg p-10">
+            <div className="flex flex-col items-end gap-2 w-full">
+              <span>{renderStatus(data.status)}</span>
+              <span className="text-md font-semibold">{data.code}</span>
+            </div>
+            <div className="grid grid-cols-2 mt-10">
+              <div>
+                <h3 className="text-sm font-semibold">Invoice from</h3>
+              </div>
+            </div>
         </div>
       </div>
     </sections>
