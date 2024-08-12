7036050b88a3a3d952508db1ec62bc283f15bfa2
farildev <memmedovfaril@gmail.com>
2024-08-12 16:54:38 +0400

    feat : Moment added

--- src/layouts/PageLayouts/MailLayout.jsx
+++ src/layouts/PageLayouts/MailLayout.jsx
@@ -1,14 +1,17 @@
 import Searchbar from "@/components/common/Searchbar";
 import mails from "@/mocks/mails";
 import mailLinks from "@/utils/mailLinks";
+import clsx from "clsx";
 import { FaPen } from "react-icons/fa";
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
+import moment from "moment";
+
 const MailLayout = () => {
   return (
     <section className="py-10">
       <div className="siteContainer">
         <h1 className="font-bold text-2xl">Mail All (23)</h1>
-        <div className="w-full bg-[#F4F6F8] rounded-lg h-full p-2 mt-10 flex justify-between gap-3">
+        <aside className="w-full bg-[#F4F6F8] rounded-lg h-full p-2 mt-10 flex justify-between gap-3">
           <div className="flex flex-col p-3 w-[20%]">
             <button className="text-md bg-black p-3 rounded-lg text-white font-bold flex items-center justify-center gap-2">
               <FaPen size={22} />
@@ -32,8 +35,13 @@ const MailLayout = () => {
             <Searchbar simple />
             <div className="flex flex-col gap-2 mt-5">
               {mails.map((mail, index) => (
-                <Link
-                  className="w-full p-2 hover:bg-grey/20 rounded-lg"
+                <NavLink
+                  className={({ isActive }) =>
+                    clsx("w-full p-2 rounded-lg", {
+                      "bg-grey/20": isActive,
+                      "bg-transparent hover:bg-grey/20 ": !isActive,
+                    })
+                  }
                   to={`${mail.id}`}
                   key={index}
                 >
@@ -45,26 +53,30 @@ const MailLayout = () => {
                     />
                     <div className="flex flex-col w-full">
                       <div className="flex justify-between gap-2 w-full">
-                        <span className="text-md font-semibold">{mail.opponent.name}</span>
-                        <span>3days</span>
+                        <span className="text-sm font-semibold">
+                          {mail.opponent.name}
+                        </span>
+                        <span className="text-gray-400 text-sm">{moment(mail.created_at).fromNow()}</span>
                       </div>
                       <div className="w-full flex items-center justify-between relative">
-                        <span className="line-clamp-1 ">{mail.message}</span>
-                        <span className="w-[10px] h-[10px] bg-blue-500 absolute right-[5%] bottom-[20%] rounded-full"></span>
+                        <span className={`line-clamp-1 text-sm ${mail.is_read ? 'text-gray-400' : 'text-black'}`}>{mail.message}</span>
+                        {!mail.is_read ? (
+                          <span className="w-[10px] h-[10px] bg-blue-500 absolute right-[5%] bottom-[20%] rounded-full"></span>
+                        ) : null}
                       </div>
                     </div>
                   </div>
-                </Link>
+                </NavLink>
               ))}
             </div>
           </div>
           <div className="flex-1 bg-white rounded-lg p-3">
             <Outlet />
           </div>
-        </div>
+        </aside>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default MailLayout
+export default MailLayout;
