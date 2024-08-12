45e6c62136387d42a46f2bbc0c70376f9aafb5b1
farildev <memmedovfaril@gmail.com>
2024-08-12 23:44:31 +0400

    feat : Textarea added to mailbox & changed size

--- src/components/common/SecondTextArea.jsx
+++ src/components/common/SecondTextArea.jsx
@@ -2,9 +2,9 @@ import PropTypes from "prop-types";
 
 const SecondTextArea = ({placeholder, label, column, solid}) => {
   return (
-    <div className={`flex ${column ? 'flex-col' : 'flex-row items-center'} gap-2 w-full`}>
+    <div className={`flex ${column ? 'flex-col' : 'flex-row items-center'} gap-2 w-full h-full`}>
       <label className="text-sm text-gray-500 font-light">{label}</label>
-      <textarea className={`w-full outline-none min-h-24 resize-none p-4 rounded-lg text-sm ${solid ? 'bg-grey/20' : 'border border-grey/20'}`} placeholder={placeholder}/>
+      <textarea className={`w-full outline-none h-full resize-none p-4 rounded-lg text-sm ${solid ? 'bg-grey/20' : 'border border-grey/20'}`} placeholder={placeholder}/>
     </div>
   )
 }
--- src/views/App/Mail/MailDetail.jsx
+++ src/views/App/Mail/MailDetail.jsx
@@ -1,15 +1,17 @@
-import { useParams } from "react-router-dom";
+import SecondTextArea from "@/components/common/SecondTextArea";
 import mails from "@/mocks/mails";
+import { useParams } from "react-router-dom";
 import { FaRegStar, FaStar } from "react-icons/fa6";
+import { IoSend } from "react-icons/io5";
 import { HiTrash } from "react-icons/hi2";
 import { LuMoreVertical } from "react-icons/lu";
-
+import { IoMdAttach } from "react-icons/io";
 const MailDetail = () => {
   const { id } = useParams();
   const selectedMail = mails.find((mail) => mail.id === parseInt(id));
 
   return (
-    <div className="h-full">
+    <div className="h-full relative">
       <div className="flex items-center justify-end w-full gap-3 py-3 border-b border-dashed border-gray-300/40">
         <button>
           {selectedMail.is_starred ? (
@@ -39,6 +41,16 @@ const MailDetail = () => {
             </span>
           </div>
           <span className="text-black text-xs">To : <span className="text-xs text-gray-400">{selectedMail.to}</span></span>
+        </div>
+      </div>
+      <div className="p-4 h-[261px] overflow-y-auto">
+          <p className="text-sm whitespace-pre-wrap">{selectedMail.message}</p>
+      </div>
+      <div className="absolute bottom-0 left-0 w-full h-[330px] z-20 flex flex-col gap-3">
+        <SecondTextArea solid placeholder="Write something awesome..." />
+        <div className="flex items-center justify-between">
+          <button className="hover:bg-gray-300 transition-all duration-300 rounded w-[25px] h-[25px] flex items-center justify-center"><IoMdAttach size={18}/></button>
+          <button className="bg-green-700 p-3 rounded-lg text-white font-semibold flex items-center gap-3 text-sm">Send <IoSend size={18}/></button>
         </div>
       </div>
     </div>
