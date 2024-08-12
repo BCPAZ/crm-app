17b797493afee9b74fc98d7f5cf42323d9ef2184
farildev <memmedovfaril@gmail.com>
2024-08-12 16:20:59 +0400

    feat : Email detail page created

--- src/views/App/Mail/MailDetail.jsx
+++ src/views/App/Mail/MailDetail.jsx
@@ -1,18 +1,46 @@
 import { useParams } from "react-router-dom";
 import mails from "@/mocks/mails";
+import { FaRegStar, FaStar } from "react-icons/fa6";
+import { HiTrash } from "react-icons/hi2";
+import { LuMoreVertical } from "react-icons/lu";
 
 const MailDetail = () => {
-  const { mailId } = useParams(); // URL'den mail ID'sini al
-  const selectedMail = mails.find((mail) => mail.id === mailId); // Seçili maili bul
-
-  if (!selectedMail) {
-    return <p>No mail selected. Please select a mail to view details.</p>; // Hiçbir mail seçilmemişse mesaj göster
-  }
+  const { id } = useParams();
+  const selectedMail = mails.find((mail) => mail.id === parseInt(id));
 
   return (
-    <div>
-      <h2 className="text-xl font-bold">{selectedMail.opponent.name}</h2>
-      <p>{selectedMail.message}</p>
+    <div className="h-full">
+      <div className="flex items-center justify-end w-full gap-3 py-3 border-b border-dashed border-gray-300/40">
+        <button>
+          {selectedMail.is_starred ? (
+            <FaStar color="#FFAB00" size={20} />
+          ) : (
+            <FaRegStar color="#FFAB00" size={20} />
+          )}
+        </button>
+        <button className="text-gray-500">
+          <HiTrash size={20} />
+        </button>
+        <button className="text-gray-500">
+          <LuMoreVertical size={20} />
+        </button>
+      </div>
+      <div className="p-4 flex items-center gap-3">
+        <img
+          className="w-[40px] h-[40px] rounded-full"
+          src={selectedMail.opponent.avatar_url}
+          alt={selectedMail.opponent.name}
+        />
+        <div>
+          <div className="flex items-center gap-2">
+            <span className="text-sm">{selectedMail.opponent.name}</span>
+            <span className="text-xs text-gray-400">
+              {`<${selectedMail.opponent.email}>`}
+            </span>
+          </div>
+          <span className="text-black text-xs">To : <span className="text-xs text-gray-400">{selectedMail.to}</span></span>
+        </div>
+      </div>
     </div>
   );
 };
