60485f8e8ccbde3bcc88d449b58e276c59233a10
farildev <memmedovfaril@gmail.com>
2024-08-12 14:11:00 +0400

    feat : Icons added

---
+++ src/utils/mailLinks.jsx
@@ -0,0 +1,15 @@
+import { IoMdMail } from "react-icons/io";
+import { FaInbox } from "react-icons/fa6";
+
+const mailLinks = [
+  {
+    label : 'All',
+    icon : <IoMdMail />
+  },
+  {
+    label : 'Inbox',
+    icon : <FaInbox />
+  }
+]
+
+export default mailLinks
--- src/views/App/Mail/Mail.jsx
+++ src/views/App/Mail/Mail.jsx
@@ -1,3 +1,4 @@
+import mailLinks from "@/utils/mailLinks";
 import { FaPen } from "react-icons/fa";
 const Mail = () => {
   return (
@@ -6,8 +7,18 @@ const Mail = () => {
         <h1 className="font-bold text-2xl">Mail All (23)</h1>
         <div className="w-full bg-[#F4F6F8] rounded-lg h-full p-2 mt-10">
           <div className="flex flex-col p-3 w-[18%]">
-            <button className="text-md bg-black p-3 rounded-lg text-white font-bold flex items-center justify-center gap-2"><FaPen size={22}/>Compose</button>
-            
+            <button className="text-md bg-black p-3 rounded-lg text-white font-bold flex items-center justify-center gap-2">
+              <FaPen size={22} />
+              Compose
+            </button>
+            <div className="p-3 flex flex-col">
+              {mailLinks.map((link, index) => (
+                <button className="flex items-center w-full gap-4 p-2" key={index}>
+                  <span className="text-2xl text-gray-500">{link.icon}</span>
+                  <span className="text-lg text-gray-500 font-medium">{link.label}</span>
+                </button>
+              ))}
+            </div>
           </div>
         </div>
       </div>
