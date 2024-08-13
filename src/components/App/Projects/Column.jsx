c2d44ebff44b3ad24213d65422c43024aefecf21
farildev <memmedovfaril@gmail.com>
2024-08-13 13:48:57 +0400

    feat : Column and Kanban design completed

--- src/components/App/Projects/Column.jsx
+++ src/components/App/Projects/Column.jsx
@@ -1,7 +1,28 @@
+import { IoAddSharp } from "react-icons/io5";
+import { GoKebabHorizontal } from "react-icons/go";
+import { MdDragIndicator } from "react-icons/md";
+
+
 const Column = () => {
   return (
-    <div className="w-[336px]">Column</div>
-  )
-}
+    <div className="w-[336px] bg-column rounded-xl py-5 px-4">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          <span className="w-[25px] h-[25px] bg-grey/40 flex text-sm rounded-full items-center justify-center">
+            3
+          </span>
+          <h1 className="text-lg font-bold">To do</h1>
+        </div>
+        <div className="flex items-center gap-3">
+          <button className="w-[25px] h-[25px] bg-black text-white flex text-sm rounded-full items-center justify-center">
+            <IoAddSharp size={18}/>
+          </button>
+          <button className="p-1 hover:bg-grey/20 rounded-full transition-all duration-300 text-gray-40"><GoKebabHorizontal size={18}/></button>
+          <button className="p-1 hover:bg-grey/20 rounded-full transition-all duration-300 text-gray-40"><MdDragIndicator size={18}/></button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Column
+export default Column;
--- src/components/App/Projects/KanbanBoard.jsx
+++ src/components/App/Projects/KanbanBoard.jsx
@@ -14,13 +14,13 @@ const KanbanBoard = () => {
   return (
     <section className="py-10">
       <div className="p-4">
-        <div className="flex space-x-4 overflow-x-auto">
+        <div className="flex space-x-6 overflow-x-auto">
           {columns.map((column) => (
             <Column key={column.id} name={column.name} />
           ))}
           <button
             onClick={handleAddColumn}
-            className="w-[336px] p-2 bg-white border border-grey/20 rounded-lg text-sm flex items-center justify-center gap-2 font-semibold"
+            className="w-[336px] h-12 p-2 bg-white border border-grey/20 rounded-lg text-sm flex items-center justify-center gap-2 font-semibold"
           >
             <IoAddSharp size={20} />
             Create Column
