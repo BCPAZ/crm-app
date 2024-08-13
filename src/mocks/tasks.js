94755cd1f3f00b72da885c07485eb1ee0a05155a
farildev <memmedovfaril@gmail.com>
2024-08-13 11:07:03 +0400

    chore : Tasks mock created

---
+++ src/mocks/tasks.js
@@ -0,0 +1,127 @@
+const tasks = [
+  {
+    id:1,
+    project : 'AZINTEST',
+    projectDesc : 'Lorem ipsum dolor sit amet',
+    tasks: [
+      {
+        id: 1,
+        type : 'TODO',
+        subtasks : [
+          {
+            title : 'Go to the gym',
+            subtitle : 'loremipsum dolor sit amet'
+          },
+          {
+            title : 'Go to the gym',
+            subtitle : 'loremipsum dolor sit amet'
+          },
+          {
+            title : 'Go to the gym',
+            subtitle : 'loremipsum dolor sit amet'
+          },
+        ]
+      },
+      {
+        id: 2,
+        type : 'PROGRESS',
+        subtasks : [
+          {
+            title : 'Go to the gym',
+            subtitle : 'loremipsum dolor sit amet'
+          },
+          {
+            title : 'Go to the gym',
+            subtitle : 'loremipsum dolor sit amet'
+          },
+          {
+            title : 'Go to the gym',
+            subtitle : 'loremipsum dolor sit amet'
+          },
+        ]
+      },
+      {
+        id: 3,
+        type : 'COMPLETED',
+        subtasks : [
+          {
+            title : 'Go to the gym',
+            subtitle : 'loremipsum dolor sit amet'
+          },
+          {
+            title : 'Go to the gym',
+            subtitle : 'loremipsum dolor sit amet'
+          },
+          {
+            title : 'Go to the gym',
+            subtitle : 'loremipsum dolor sit amet'
+          },
+        ]
+      },
+    ]
+  },
+  {
+    id:2,
+    project : 'BCP.AZ',
+    projectDesc : 'Lorem ipsum dolor sit amet',
+    tasks: [
+      {
+        id: 1,
+        type : 'TODO',
+        subtasks : [
+          {
+            title : 'Go to the gym',
+            subtitle : 'loremipsum dolor sit amet'
+          },
+          {
+            title : 'Go to the gym',
+            subtitle : 'loremipsum dolor sit amet'
+          },
+          {
+            title : 'Go to the gym',
+            subtitle : 'loremipsum dolor sit amet'
+          },
+        ]
+      },
+      {
+        id: 2,
+        type : 'PROGRESS',
+        subtasks : [
+          {
+            title : 'Go to the gym',
+            subtitle : 'loremipsum dolor sit amet'
+          },
+          {
+            title : 'Go to the gym',
+            subtitle : 'loremipsum dolor sit amet'
+          },
+          {
+            title : 'Go to the gym',
+            subtitle : 'loremipsum dolor sit amet'
+          },
+        ]
+      },
+      {
+        id: 3,
+        type : 'COMPLETED',
+        subtasks : [
+          {
+            title : 'Go to the gym',
+            subtitle : 'loremipsum dolor sit amet'
+          },
+          {
+            title : 'Go to the gym',
+            subtitle : 'loremipsum dolor sit amet'
+          },
+          {
+            title : 'Go to the gym',
+            subtitle : 'loremipsum dolor sit amet'
+          },
+        ]
+      },
+    ]
+  },
+]
+
+
+export default tasks
