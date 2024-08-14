fe03b07b2c7b3b571c8965499ffe999ec65f1635
farildev <memmedovfaril@gmail.com>
2024-08-14 17:02:15 +0400

    refactor : TaskCard component created & Column component refactored

--- src/components/App/Projects/Column.jsx
+++ src/components/App/Projects/Column.jsx
@@ -1,24 +1,13 @@
 import PropTypes from "prop-types";
-import { Droppable, Draggable } from "@hello-pangea/dnd";
+import { Droppable } from "@hello-pangea/dnd";
 import { IoAddSharp } from "react-icons/io5";
-import { CgAttachment } from "react-icons/cg";
 import { MdDragIndicator } from "react-icons/md";
 import { HiTrash } from "react-icons/hi2";
-import { FaCommentDots } from "react-icons/fa6";
-import High from "@/assets/icons/Kanban/high.svg";
-import Medium from "@/assets/icons/Kanban/medium.svg";
-import Low from "@/assets/icons/Kanban/low.svg";
+
 import kanbanData from "@/mocks/kanbanData";
+import TaskCard from "./TaskCard";
 const Column = ({ column, handleDeleteColumn }) => {
-  const checkPriority = (priority) => {
-    if (priority === "High") {
-      return High;
-    } else if (priority === "Medium") {
-      return Medium;
-    } else {
-      return Low;
-    }
-  };
+  
   return (
     <Droppable droppableId={column.id} type="TASK">
       {(provided) => (
@@ -52,55 +41,7 @@ const Column = ({ column, handleDeleteColumn }) => {
           <div className="flex flex-col gap-4">
             {kanbanData &&
               kanbanData.map((task, index) => (
-                <Draggable key={task.id} draggableId={task.id} index={index}>
-                  {(provided) => (
-                    <div
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                      className="bg-white rounded-xl"
-                    >
-                      <div className="flex flex-col">
-                        <div className="h-[200px] overflow-hidden w-full p-2">
-                          <img
-                            className="w-full h-full object-cover rounded-lg"
-                            src={task.img}
-                            alt={task.title}
-                          />
-                        </div>
-                        <div className="w-full flex justify-end px-5">
-                          <img
-                            className="w-[25px]"
-                            src={checkPriority(task.priority)}
-                            alt="Priority Level"
-                          />
-                        </div>
-                        <div className="pb-5 px-5 mt-2">
-                          <h3 className="text-md font-bold">{task.title}</h3>
-                          <div className="flex items-center justify-between gap-2">
-                            <div className="flex items-center gap-2">
-                              <button className="flex items-center gap-1 text-gray-500 text-sm font-semibold">
-                                <FaCommentDots size={14} />
-                                <span>{task.comments?.length}</span>
-                              </button>
-                              <button className="flex items-center gap-1 text-gray-500 text-sm font-semibold">
-                                <CgAttachment size={14} />
-                                <span>{task.attachments?.length}</span>
-                              </button>
-                            </div>
-                            <div className="flex items-center">
-                              {task.selectedUsers?.map((user, index) => (
-                                <div className="w-[25px] h-[25px] rounded-full overflow-hidden border border-white" key={index}>
-                                  <img className="w-full h-full object-cover object-center" src={user.img} alt={user.name} />
-                                </div>
-                              ))}
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  )}
-                </Draggable>
+                <TaskCard task={task} key={index} index={index} />
               ))}
             {provided.placeholder}
           </div>
---
+++ src/components/App/Projects/TaskCard.jsx
@@ -0,0 +1,95 @@
+import { Draggable } from "@hello-pangea/dnd";
+import { FaCommentDots } from "react-icons/fa6";
+import { CgAttachment } from "react-icons/cg";
+import PropTypes from "prop-types";
+import High from "@/assets/icons/Kanban/high.svg";
+import Medium from "@/assets/icons/Kanban/medium.svg";
+import Low from "@/assets/icons/Kanban/low.svg";
+
+const TaskCard = ({ task ,index }) => {
+
+  const checkPriority = (priority) => {
+    if (priority === "High") {
+      return High;
+    } else if (priority === "Medium") {
+      return Medium;
+    } else {
+      return Low;
+    }
+  };
+  return (
+    <Draggable key={task.id} draggableId={task.id} index={index}>
+      {(provided) => (
+        <div
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          className="bg-white rounded-xl"
+        >
+          <div className="flex flex-col">
+            <div className="h-[200px] overflow-hidden w-full p-2">
+              <img
+                className="w-full h-full object-cover rounded-lg"
+                src={task.img}
+                alt={task.title}
+              />
+            </div>
+            <div className="w-full flex justify-end px-5">
+              <img
+                className="w-[25px]"
+                src={checkPriority(task.priority)}
+                alt="Priority Level"
+              />
+            </div>
+            <div className="pb-5 px-5 mt-2">
+              <h3 className="text-md font-bold">{task.title}</h3>
+              <div className="flex items-center justify-between gap-2">
+                <div className="flex items-center gap-2">
+                  <button className="flex items-center gap-1 text-gray-500 text-sm font-semibold">
+                    <FaCommentDots size={14} />
+                    <span>{task.comments?.length}</span>
+                  </button>
+                  <button className="flex items-center gap-1 text-gray-500 text-sm font-semibold">
+                    <CgAttachment size={14} />
+                    <span>{task.attachments?.length}</span>
+                  </button>
+                </div>
+                <div className="flex items-center">
+                  {task.selectedUsers?.map((user, index) => (
+                    <div
+                      className="w-[25px] h-[25px] rounded-full overflow-hidden border border-white"
+                      key={index}
+                    >
+                      <img
+                        className="w-full h-full object-cover object-center"
+                        src={user.img}
+                        alt={user.name}
+                      />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </Draggable>
+  );
+};
+
+
+TaskCard.propTypes = {
+  task: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    img: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    priority: PropTypes.string.isRequired,
+    comments: PropTypes.array.isRequired,
+    attachments: PropTypes.arrayOf(PropTypes.string),
+    selectedUsers: PropTypes.arrayOf(PropTypes.string),
+  }).isRequired,
+  index: PropTypes.number.isRequired,
+};
+
+
+export default TaskCard;
