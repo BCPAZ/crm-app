2e0802b66d032d6c0810b56f26a00fff1dee573f
farildev <memmedovfaril@gmail.com>
2024-08-14 16:27:46 +0400

    chore : kanban data created

---
+++ src/mocks/kanbanData.js
@@ -0,0 +1,63 @@
+const tasks = [
+  {
+    id: 1,
+    title: "Close image",
+    img: "https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_1200,h_630/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/tsah7c9evnal289z5fig/IMG%20Worlds%20of%20Adventure%20Admission%20Ticket%20in%20Dubai%20-%20Klook.jpg",
+    priority: "High",
+    comments : [
+      {
+        id:1,
+        name : 'Sarah Gille',
+        img : 'https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671142.jpg',
+        comment : 'Helal olsun dayi ogliiiiii'
+      },
+      {
+        id:2,
+        name : 'Elsen Memmedov',
+        img : 'https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671142.jpg',
+        comment : 'Helal olsun dayi ogliiiiii'
+      },
+      {
+        id:3,
+        name : 'Salam',
+        img : 'https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671142.jpg',
+        comment : 'Helal olsun dayi ogliiiiii'
+      },
+    ],
+    selectedUsers : [
+      {
+        id:1,
+        name : 'Sarah Gille',
+        img : 'https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671142.jpg',
+        comment : 'Helal olsun dayi ogliiiiii'
+      },
+      {
+        id:2,
+        name : 'Elsen Memmedov',
+        img : 'https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671142.jpg',
+        comment : 'Helal olsun dayi ogliiiiii'
+      },
+      {
+        id:3,
+        name : 'Salam',
+        img : 'https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671142.jpg',
+        comment : 'Helal olsun dayi ogliiiiii'
+      },
+    ]
+  },
+  {
+    id: 2,
+    title: "Close image",
+    img: "https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_1200,h_630/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/tsah7c9evnal289z5fig/IMG%20Worlds%20of%20Adventure%20Admission%20Ticket%20in%20Dubai%20-%20Klook.jpg",
+    priority: "Medium",
+  },
+  {
+    id: 3,
+    title: "Delete functionality",
+    img: "https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_1200,h_630/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/tsah7c9evnal289z5fig/IMG%20Worlds%20of%20Adventure%20Admission%20Ticket%20in%20Dubai%20-%20Klook.jpg",
+    priority: "Low",
+  },
+];
+
+
+export default tasks
