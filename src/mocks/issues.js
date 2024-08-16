80b9d3f929ebc45196fa04a4f67178cddb9ec724
farildev <memmedovfaril@gmail.com>
2024-08-16 15:31:22 +0400

    chore : Issues mock created

---
+++ src/mocks/issues.js
@@ -0,0 +1,28 @@
+const issues = [
+  {
+    id:1,
+    image : 'https://t3.ftcdn.net/jpg/03/31/59/82/360_F_331598222_tadTdFeI0WxEmyGFoWuMTlEK1jNu2Xhl.jpg',
+    title : 'Construction problem happened',
+    user: 'Jayvion Simon',
+    assignee : 'Anar Anar',
+    created_at: "2024-08-10T08:32:05.000000Z",
+    created_time : '10:00',
+    checked_time : '10:00',
+    checked_at: "2024-08-10T08:32:05.000000Z",
+    status : 'resolved'
+  },
+  {
+    id:2,
+    image : 'https://t3.ftcdn.net/jpg/03/31/59/82/360_F_331598222_tadTdFeI0WxEmyGFoWuMTlEK1jNu2Xhl.jpg',
+    title : 'Construction problem happened',
+    user: 'Jayvion Simon',
+    assignee : 'Anar Anar',
+    created_at: "2024-08-10T08:32:05.000000Z",
+    created_time : '10:00',
+    checked_time : '10:00',
+    checked_at: "2024-08-10T08:32:05.000000Z",
+    status : 'pending'
+  },
+]
+
+export default issues
