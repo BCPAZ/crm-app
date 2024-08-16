53e495a82927495cd563925206ba440eb51d2322
farildev <memmedovfaril@gmail.com>
2024-08-16 13:43:20 +0400

    feat : Input style changed & text center added

--- src/views/App/FieldManagement/Reports.jsx
+++ src/views/App/FieldManagement/Reports.jsx
@@ -6,13 +6,13 @@ const Reports = () => {
   return (
     <section className="p-10">
       <div className="grid lg:grid-cols-2 gap-10">
-        <div className="h-[250px] bg-grey/10 rounded-xl flex flex-col items-center">
+        <div className="h-[250px] bg-grey/10 p-5 rounded-xl flex flex-col items-center">
           <img src={file} alt="File Icon" />
           <div className="flex items-center flex-col gap-2">
             <h4 className="text-md font-semibold text-center">
               Drop or select file
             </h4>
-            <p className="text-xs font-medium text-gray-500">
+            <p className="text-xs font-medium text-center text-gray-500">
               Drop files here or click to{" "}
               <span className="text-green-600">browse</span> through your
               machine.
@@ -21,7 +21,7 @@ const Reports = () => {
         </div>
         <div className="flex flex-col gap-4">
           <div className="lg:w-1/2 w-full">
-            <SecondInput placeholder="Description" />
+            <SecondInput column placeholder="Description" />
           </div>
           <div className="lg:w-1/2 w-full">
             <Select />
