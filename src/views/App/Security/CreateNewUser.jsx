13957fae5efe5282262cdbacef5be9532acc7d44
farildev <memmedovfaril@gmail.com>
2024-08-17 16:32:28 +0400

    feat : page's layout created

--- src/views/App/Security/CreateNewUser.jsx
+++ src/views/App/Security/CreateNewUser.jsx
@@ -2,7 +2,15 @@ const CreateNewUser = () => {
   return (
     <section className="w-full h-full">
       <div className="siteContainer">
-        salam
+        <h1 className="text-2xl font-semibold">Create new user</h1>
+        <div className="flex">
+          <div>
+
+          </div>
+          <div>
+
+          </div>
+        </div>
       </div>
     </section>
   )
