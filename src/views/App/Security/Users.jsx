fee564eeaf736fc9aaa6686eb78a4e964dee93eb
farildev <memmedovfaril@gmail.com>
2024-08-17 16:32:52 +0400

    chore : Path changed

--- src/views/App/Security/Users.jsx
+++ src/views/App/Security/Users.jsx
@@ -10,7 +10,7 @@ const Users = () => {
           <UserList />
         </div>
         <div className="mt-5 flex justify-end">
-          <Link to={'/create-user'} className="bg-black p-3 font-semibold text-white rounded-lg text-sm flex items-center gap-2">
+          <Link to={'/create-new-user'} className="bg-black p-3 font-semibold text-white rounded-lg text-sm flex items-center gap-2">
             <IoAddSharp size={18} />
             Create Role
           </Link>
