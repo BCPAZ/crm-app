46aed383b5c3d8b4d41a060191a5bef399a61d9b
farildev <memmedovfaril@gmail.com>
2024-08-17 13:05:26 +0400

    feat : Job title page created

--- src/views/App/Security/JobTitle.jsx
+++ src/views/App/Security/JobTitle.jsx
@@ -1,6 +1,13 @@
 const JobTitle = () => {
   return (
-    <div>JobTitle</div>
+    <section className="w-full h-full py-5">
+        <div className="siteContainer">
+        <h1 className="text-2xl font-semibold">Job title</h1>
+          <div className="mt-10 w-full">
+            salam
+          </div>
+        </div>
+      </section>
   )
 }
 
