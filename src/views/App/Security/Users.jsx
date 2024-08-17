d11a271ca8697ad12d4b66abbc48429e4cee278f
farildev <memmedovfaril@gmail.com>
2024-08-17 16:12:20 +0400

    feat : Users page completed

--- src/views/App/Security/Users.jsx
+++ src/views/App/Security/Users.jsx
@@ -1,15 +1,23 @@
-import UserList from "@/components/App/Security/UserList"
+import UserList from "@/components/App/Security/UserList";
+import { IoAddSharp } from "react-icons/io5";
+import { Link } from "react-router-dom";
 const Users = () => {
   return (
-      <section className="w-full h-full py-5">
-        <div className="mx-auto w-full">
+    <section className="w-full h-full py-5">
+      <div className="mx-auto w-full">
         <h1 className="text-2xl font-semibold">Users list</h1>
-          <div className="mt-10 w-full">
-            <UserList />
-          </div>
+        <div className="mt-10 w-full">
+          <UserList />
         </div>
-      </section>
-  )
-}
+        <div className="mt-5 flex justify-end">
+          <Link to={'/create-user'} className="bg-black p-3 font-semibold text-white rounded-lg text-sm flex items-center gap-2">
+            <IoAddSharp size={18} />
+            Create Role
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Users
+export default Users;
