96aaa6705d63ef68ad54379d65710d862f1f6a55
farildev <memmedovfaril@gmail.com>
2024-08-17 11:42:30 +0400

    feat : Security layout completed

--- src/layouts/PageLayouts/SecurityLayout.jsx
+++ src/layouts/PageLayouts/SecurityLayout.jsx
@@ -1,11 +1,30 @@
-import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom";
+import Sidebar from "@/components/App/Documents/Sidebar";
+import { navigationLinks } from "@/utils/constants";
 
 const SecurityLayout = () => {
+  const securityLinks = navigationLinks.find((link) => link.title.toLowerCase() === 'security').elements;
   return (
-    <main>
-      <Outlet />
-    </main>
-  )
-}
+    <div className="h-full flex flex-col">
+      <div className="border-b border-gray-400 py-7">
+        <div className="siteContainer">
+          <div className="flex items-center justify-between">
+            <h1 className="text-xl font-semibold w-full">Field Management</h1>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-grow">
+        <div className="w-[20%] h-screen lg:block hidden">
+          <Sidebar links={securityLinks} />
+        </div>
+        <main
+          className={`h-full w-[75%] "}`}
+        >
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+};
 
-export default SecurityLayout
+export default SecurityLayout;
