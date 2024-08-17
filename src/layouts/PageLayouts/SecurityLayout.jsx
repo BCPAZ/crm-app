e7b762a18be4c83e97c2ad78e19875f875738109
farildev <memmedovfaril@gmail.com>
2024-08-17 16:26:25 +0400

    feat : Sidebar condition updated

--- src/layouts/PageLayouts/SecurityLayout.jsx
+++ src/layouts/PageLayouts/SecurityLayout.jsx
@@ -1,15 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "@/components/App/Documents/Sidebar";
 import { navigationLinks } from "@/utils/constants";
-
+import { useLocation } from "react-router-dom";
 const SecurityLayout = () => {
   const securityLinks = navigationLinks.find((link) => link.title.toLowerCase() === 'security').elements;
+  const location = useLocation();
+
+  const hideSidebar = location.pathname === '/create-new-user';
   return (
     <div className="h-full flex flex-col">
       <div className="flex gap-3">
-        <div className="w-[250px] h-screen lg:block hidden">
+        {!hideSidebar && <div className="w-[250px] h-screen lg:block hidden">
           <Sidebar links={securityLinks} />
-        </div>
+        </div>}
         <main
           className={`h-full lg:max-w-[75%] w-full px-5`}
         >
