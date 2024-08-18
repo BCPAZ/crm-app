f618e013c093cdaf896adfe4d2b96086f393609b
farildev <memmedovfaril@gmail.com>
2024-08-19 00:15:10 +0400

    feat : Create user modal implemented

--- src/components/App/Security/CreateRoleModal.jsx
+++ src/components/App/Security/CreateRoleModal.jsx
@@ -1,10 +1,11 @@
 import Alert from "@/components/common/Alert"
 import Input from "@/components/common/Input"
-import Select from "@/components/common/Select"
+import Select from "@/components/common/Select";
+import PropTypes from "prop-types";
 
-const CreateRoleModal = () => {
+const CreateRoleModal = ({showModal, closeModal}) => {
   return (
-    <div className="w-full h-screen bg-black/70 flex items-center justify-center z-20 fixed top-0 left-0 right-0 bottom-0">
+    <div className={`w-full h-screen bg-black/70 ${showModal ? 'flex' : 'hidden'} items-center justify-center z-20 fixed top-0 left-0 right-0 bottom-0`}> 
       <div className="bg-white p-6 rounded-xl shadow-lg flex flex-col gap-5">
         <h1 className="text-xl font-semibold">Create new role</h1>
         <Alert />
@@ -12,11 +13,16 @@ const CreateRoleModal = () => {
         <Select column/>
         <div className="flex justify-end gap-3">
           <button className="text-sm p-2 border border-grey/20 rounded-lg font-semibold bg-black text-white">Create </button>
-          <button className="text-sm p-2 border border-grey/40 rounded-lg font-semibold bg-white">Cancel</button>
+          <button onClick={closeModal} className="text-sm p-2 border border-grey/40 rounded-lg font-semibold bg-white">Cancel</button>
         </div>
       </div>
     </div>
   )
+}
+
+CreateRoleModal.propTypes = {
+  showModal : PropTypes.bool,
+  closeModal : PropTypes.func
 }
 
 export default CreateRoleModal
--- src/views/App/Security/JobTitle.jsx
+++ src/views/App/Security/JobTitle.jsx
@@ -1,13 +1,21 @@
 import { IoAddSharp } from "react-icons/io5";
 import FileTable from "@/components/common/FileTable";
+import { useState } from "react";
 import CreateRoleModal from "@/components/App/Security/CreateRoleModal";
 const JobTitle = () => {
+  const [showModal, setShowModal] = useState(false);
+  const handleModal = () => {
+    setShowModal(true);
+  }
+  const closeModal = () => {
+    setShowModal(false)
+  }
   return (
     <section className="w-full h-full relative">
       <div className="py-10">
         <div className="flex justify-between items-center gap-2">
         <h1 className="text-2xl font-semibold">Roles</h1>
-        <button className="bg-black p-3 font-semibold text-white rounded-lg text-sm flex items-center gap-2">
+        <button onClick={handleModal} className="bg-black p-3 font-semibold text-white rounded-lg text-sm flex items-center gap-2">
           <IoAddSharp size={18} />
           Create Role
         </button>
@@ -15,9 +23,8 @@ const JobTitle = () => {
         <div className="mt-10">
           <FileTable />
         </div>
-        
       </div>
-      <CreateRoleModal />
+      <CreateRoleModal showModal={showModal} closeModal={closeModal} /> 
     </section>
   )
 }
