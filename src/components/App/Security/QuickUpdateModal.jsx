0eed5725db17aaee3d62ed61dfe93171fdf98336
farildev <memmedovfaril@gmail.com>
2024-08-19 00:40:16 +0400

    feat : Quick update modal implemented

--- src/components/App/Security/QuickUpdateModal.jsx
+++ src/components/App/Security/QuickUpdateModal.jsx
@@ -7,12 +7,23 @@ const QuickUpdateModal = ({showModal, closeModal}) => {
   return (
     <div className={`w-full h-screen bg-black/70 ${showModal ? 'flex' : 'hidden'} items-center justify-center z-20 fixed top-0 left-0 right-0 bottom-0`}> 
       <div className="bg-white p-6 rounded-xl shadow-lg flex flex-col gap-5">
-        <h1 className="text-xl font-semibold">Quick update</h1>
+        <h1 className="text-xl font-semibold">Quick Update</h1>
         <Alert />
-        <Input label="Position name" type="text" placeholder="Add position name"/>
-        <Select column/>
+        <div className="w-1/2">
+          <Select />
+        </div>
+        <div className="grid grid-cols-2 gap-5">
+          <Input label="Full name" type="text" placeholder="Enter fullname"/>
+          <Input label="Email address" type="text" placeholder="Enter email"/>
+          <Input label="Phone number" type="text" placeholder="Enter phone number"/>
+          <Select />
+          <Input label="Address" type="text" placeholder="Enter address"/>
+          <Input label="Zip/Code" type="text" placeholder="Enter Zip Code"/>
+          <Input label="Password" type="password" placeholder="Enter password"/>
+          <Select />
+        </div>
         <div className="flex justify-end gap-3">
-          <button className="text-sm p-2 border border-grey/20 rounded-lg font-semibold bg-black text-white">Create</button>
+          <button className="text-sm p-2 border border-grey/20 rounded-lg font-semibold bg-black text-white">Update</button>
           <button onClick={closeModal} className="text-sm p-2 border border-grey/40 rounded-lg font-semibold bg-white">Cancel</button>
         </div>
       </div>
