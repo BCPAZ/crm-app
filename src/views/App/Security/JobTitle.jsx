5e1d1f6ea57a6b041ae40966a856659dcd73d4fc
farildev <memmedovfaril@gmail.com>
2024-08-19 00:27:19 +0400

    feat : Add position modal added to job title page

--- src/views/App/Security/JobTitle.jsx
+++ src/views/App/Security/JobTitle.jsx
@@ -1,7 +1,7 @@
 import { IoAddSharp } from "react-icons/io5";
 import FileTable from "@/components/common/FileTable";
 import { useState } from "react";
-import CreateRoleModal from "@/components/App/Security/CreateRoleModal";
+import JobTitleModal from "@/components/App/Security/JobTitleModal";
 const JobTitle = () => {
   const [showModal, setShowModal] = useState(false);
   const handleModal = () => {
@@ -14,17 +14,17 @@ const JobTitle = () => {
     <section className="w-full h-full relative">
       <div className="py-10 px-5">
         <div className="flex justify-between items-center gap-2">
-        <h1 className="text-2xl font-semibold">Roles</h1>
+        <h1 className="text-2xl font-semibold">Positions</h1>
         <button onClick={handleModal} className="bg-black p-3 font-semibold text-white rounded-lg text-sm flex items-center gap-2">
           <IoAddSharp size={18} />
-          Create Role
+          Create position
         </button>
         </div>
         <div className="mt-10">
           <FileTable />
         </div>
       </div>
-      <CreateRoleModal showModal={showModal} closeModal={closeModal} /> 
+      <JobTitleModal showModal={showModal} closeModal={closeModal} /> 
     </section>
   )
 }
