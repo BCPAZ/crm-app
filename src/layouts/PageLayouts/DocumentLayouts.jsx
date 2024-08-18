fd2ae1ccab49c8ea5050835e4f4c30bb7de16b79
farildev <memmedovfaril@gmail.com>
2024-08-19 00:52:07 +0400

    fix : layout design fixed

--- src/layouts/PageLayouts/DocumentLayouts.jsx
+++ src/layouts/PageLayouts/DocumentLayouts.jsx
@@ -5,7 +5,7 @@ import Sidebar from "@/components/App/Documents/Sidebar";
 const DocumentLayout = () => {
   const documentLinks = navigationLinks.find((link) => link.title.toLowerCase() === 'documents').elements;
   return (
-    <div className="h-screen flex flex-col">
+    <div className="w-full h-full flex flex-col">
       <div className="border-b border-gray-400 py-7">
         <div className="siteContainer">
           <div className="flex items-center justify-between">
@@ -14,10 +14,10 @@ const DocumentLayout = () => {
         </div>
       </div>
       <div className="flex flex-grow">
-        <div className="w-[25%] h-full">
+        <div className="w-[20%] h-screen">
           <Sidebar links={documentLinks} />
         </div>
-        <main className="w-[75%] h-full">
+        <main className="w-[80%] h-full">
           <Outlet />
         </main>
       </div>
