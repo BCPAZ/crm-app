5e020fa6cd94e6e1f8b2f700165e063172f16908
farildev <memmedovfaril@gmail.com>
2024-08-19 00:16:37 +0400

    fix : Jobtitle page fixed

--- src/views/App/Security/JobTitle.jsx
+++ src/views/App/Security/JobTitle.jsx
@@ -12,7 +12,7 @@ const JobTitle = () => {
   }
   return (
     <section className="w-full h-full relative">
-      <div className="py-10">
+      <div className="py-10 px-5">
         <div className="flex justify-between items-center gap-2">
         <h1 className="text-2xl font-semibold">Roles</h1>
         <button onClick={handleModal} className="bg-black p-3 font-semibold text-white rounded-lg text-sm flex items-center gap-2">
