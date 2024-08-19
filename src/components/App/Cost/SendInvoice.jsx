341d035f202441c6160c185b0e0e64e820975490
e1sndev <111046079+e1sndev@users.noreply.github.com>
2024-08-19 15:29:57 +0400

    fix: Style bugs fixed

--- src/components/App/Cost/SendInvoice.jsx
+++ src/components/App/Cost/SendInvoice.jsx
@@ -1,35 +1,35 @@
-import { FaPen } from "react-icons/fa";
-const SendInvoice = ({openUserModal}) => {
+import { FaPen } from 'react-icons/fa';
+const SendInvoice = ({ openUserModal }) => {
   return (
-    <div className="relative grid md:grid-cols-2 grid-cols-1 gap-8 w-full">
-      <div className="flex flex-col w-full px-6">
-        <div className="flex items-center justify-between gap-3">
-          <h1 className="text-lg text-gray-400 font-semibold">From:</h1>
-          <button onClick={openUserModal} className="text-gray-500 cursor-pointer">
+    <div className='relative grid md:grid-cols-2 grid-cols-1 w-full'>
+      {/* <div className='absolute left-[50%] translate-x-[-50%] md:flex hidden items-center'>
+        <div className='w-0.5 h-full bg-gray-200 mx-auto'></div>
+      </div> */}
+      <div className='flex flex-col w-full px-10 md:border-r border-gray-200'>
+        <div className='flex items-center justify-between gap-3'>
+          <h1 className='text-lg text-gray-400 font-semibold'>From:</h1>
+          <button onClick={openUserModal} className='text-gray-500 cursor-pointer' type='button'>
             <FaPen size={20} />
           </button>
         </div>
-        <div className="flex flex-col gap-2 text-sm mt-2">
-          <h3 className="text-black font-medium">Jayvion Simon</h3>
+        <div className='flex flex-col gap-2 text-sm mt-2'>
+          <h3 className='text-black font-medium'>Jayvion Simon</h3>
           <p>19034 Verna Unions Apt. 164 - Honolulu, RI / 87535</p>
           <span>+1 202-555-0143</span>
         </div>
       </div>
-      <div className="flex flex-col w-full px-6">
-        <div className="flex items-center justify-between gap-3">
-          <h1 className="text-lg text-gray-400 font-semibold">To:</h1>
-          <button className="text-gray-500 cursor-pointer">
+      <div className='flex flex-col w-full px-10'>
+        <div className='flex items-center justify-between gap-3'>
+          <h1 className='text-lg text-gray-400 font-semibold'>To:</h1>
+          <button className='text-gray-500 cursor-pointer' type='button'>
             <FaPen size={20} />
           </button>
         </div>
-        <div className="flex flex-col gap-2 text-sm mt-2">
-          <h3 className="text-black font-medium">Jayvion Simon</h3>
+        <div className='flex flex-col gap-2 text-sm mt-2'>
+          <h3 className='text-black font-medium'>Jayvion Simon</h3>
           <p>19034 Verna Unions Apt. 164 - Honolulu, RI / 87535</p>
           <span>+1 202-555-0143</span>
         </div>
-      </div>
-      <div className="absolute inset-0 md:flex hidden items-center">
-        <div className="w-0.5 h-full bg-gray-200 mx-auto"></div>
       </div>
     </div>
   );
--- src/components/App/ProfileCard.jsx
+++ src/components/App/ProfileCard.jsx
@@ -1,41 +1,47 @@
-import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
-import { Link } from "react-router-dom";
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
+import { Link } from 'react-router-dom';
 export default function ProfileCard() {
   return (
-    <div className="relative overflow-hidden">
+    <div className='relative overflow-hidden'>
       <Menu as='nav'>
-        <MenuButton className="flex w-[40px] h-[40px] overflow-hidden p-2 border-2 border-gray-400/20 rounded-full items-center justify-center font-semibold text-white">
+        <MenuButton className='flex w-[40px] h-[40px] overflow-hidden p-2 border-2 border-gray-400/20 rounded-full items-center justify-center font-semibold text-white'>
           <img
-            className="w-full h-full object-cover object-center"
-            src="https://img.freepik.com/premium-photo/graphic-designer-digital-avatar-generative-ai_934475-9292.jpg"
-            alt=""
+            className='w-full h-full object-cover object-center'
+            src='https://img.freepik.com/premium-photo/graphic-designer-digital-avatar-generative-ai_934475-9292.jpg'
+            alt=''
           />
         </MenuButton>
 
         <MenuItems
           transition
-          anchor="bottom end"
-          className="w-52 flex flex-col overflow-hidden max-h-60 shadow-lg absolute right-0 mt-2 rounded-xl border bg-white p-1 text-sm font-medium text-black transition duration-100 ease-out focus:outline-none"
+          anchor='bottom end'
+          className='w-52 flex flex-col !overflow-hidden max-h-60 shadow-lg relative right-0 mt-2 rounded-xl border bg-white p-1 text-sm font-medium text-black transition duration-100 ease-out focus:outline-none'
         >
-          <div className="flex flex-col gap-1 p-4 border-b border-dashed">
-            <span className="text-sm text-black">Anar Anar</span>
-            <span className="text-sm text-gray-500">anar@flegrei.ru</span>
+          <div className='flex flex-col gap-1 p-4 border-b border-dashed'>
+            <span className='text-sm text-black'>Anar Anar</span>
+            <span className='text-sm text-gray-500'>anar@flegrei.ru</span>
           </div>
-          <div className="bg-secondary h-[100px] w-[100px] rounded-full blur-[50px] absolute -top-[25%] -right-[10%]"></div>
-          <div className="border-b border-dashed py-2">
+          <div className='bg-secondary h-[100px] w-[100px] rounded-full blur-[50px] absolute -top-[25%] -right-[10%]'></div>
+          <div className='border-b border-dashed py-2'>
             <MenuItem>
-              <Link to="/" className="flex w-full items-center gap-2 rounded-lg px-4 py-3 hover:bg-grey/20">
+              <Link
+                to='/'
+                className='flex w-full items-center gap-2 rounded-lg px-4 py-3 hover:bg-grey/20'
+              >
                 Home
               </Link>
             </MenuItem>
             <MenuItem>
-              <Link to="/change-password" className="flex w-full items-center gap-2 rounded-lg px-4 py-3 hover:bg-grey/20">
+              <Link
+                to='/change-password'
+                className='flex w-full items-center gap-2 rounded-lg px-4 py-3 hover:bg-grey/20'
+              >
                 Change password
               </Link>
             </MenuItem>
           </div>
           <MenuItem>
-            <button className="text-red-600 p-4 text-start">Logout</button>
+            <button className='text-red-600 p-4 text-start'>Logout</button>
           </MenuItem>
         </MenuItems>
       </Menu>
