dd27dea942bb1f3e75f3219529e0aca73a121985
farildev <memmedovfaril@gmail.com>
2024-08-19 11:38:08 +0400

    feat : Template table completed

--- src/components/App/Workflow/TemplateTable.jsx
+++ src/components/App/Workflow/TemplateTable.jsx
@@ -1,73 +1,63 @@
 import CheckboxElement from "@/components/common/CheckboxElement";
-import usersData from "@/mocks/userData";
+import templates from "@/mocks/templates";
 import { GoArrowDown } from "react-icons/go";
 import { Link } from "react-router-dom";
-import { LuMoreVertical } from "react-icons/lu";
+import { HiMiniTrash } from "react-icons/hi2";
 
 const TemplateTable = () => {
   return (
     <div className="w-full overflow-x-auto">
-      <table className="w-full text-sm min-w-[1200px]">
+      <table className="w-full text-sm min-w-[1200px] border">
         <thead className="bg-gray-300/30 w-full rounded-lg text-left">
           <tr className="p-5 w-full flex items-center justify-between gap-5">
-            <th className="text-sm font-medium text-gray-500 flex items-center gap-3 rounded-s-lg w-[55%]">
+            <th className="text-sm font-medium text-gray-500 flex items-center gap-3 rounded-s-lg w-[50%]">
               <CheckboxElement />
               <span className="flex items-center gap-2">
                 Template name <GoArrowDown />
               </span>
             </th>
-            <th className="text-sm font-medium w-[12%] text-gray-500">
+            <th className="text-sm font-medium w-[20%] text-gray-500">
               Total day
             </th>
-            <th className="text-sm font-medium w-[12%] text-gray-500">
+            <th className="text-sm font-medium w-[20%] text-gray-500">
               Organization
             </th>
-            <th className="text-sm font-medium w-[5%] text-gray-500 rounded-e-lg">
+            <th className="text-sm font-medium w-[10%] text-gray-500 rounded-e-lg">
               Delete
             </th>
           </tr>
         </thead>
         <tbody className="w-full flex flex-col text-left">
-          {usersData.map((user, index) => (
+          {templates.map((template, index) => (
             <div className="group" key={index}>
               <tr className="p-5 border-b group-hover:bg-gray-200/20 border-grey/20 border-dashed  w-full flex items-center justify-between gap-5 min-h-[76px]">
-                <th className="text-sm font-medium text-gray-500 flex items-center gap-3 rounded-s-lg w-[35%]">
+                <th className="text-sm font-medium text-gray-500 flex items-center gap-3 rounded-s-lg w-[50%]">
                   <CheckboxElement />
                   <div className="flex items-center gap-4">
-                    <img
-                      className="w-[40px] h-[40px] rounded-full"
-                      src={user.avatar}
-                      alt={user.name}
-                    />
-                    <div className="flex flex-col">
-                      <Link to={`${user.id}`} className="text-sm text-secondary hover:underline">{user.name}</Link>
-                      <span className="text-xs text-gray-400">
-                        {user.email}
-                      </span>
-                    </div>
+                    <Link
+                      to={`${template.id}`}
+                      className="text-sm text-secondary hover:underline"
+                    >
+                      {template.title}
+                    </Link>
                   </div>
                 </th>
-                <td className="text-sm font-medium text-gray-500 w-[12%]">
+                <td className="text-sm font-medium text-gray-500 w-[20%]">
                   <div className="flex flex-col">
-                    <h3 className="text-xs text-secondary">
-                      {user.phone}
+                    <h3 className="text-sm text-secondary ">{template.totalDay}</h3>
+                  </div>
+                </td>
+                <td className="text-sm font-medium text-gray-500 w-[20%]">
+                  <div className="flex flex-col">
+                    <h3 className="text-sm text-secondary">
+                      {template.organization}
                     </h3>
                   </div>
                 </td>
-                <td className="text-sm font-medium text-gray-500 w-[12%]">
-                  <div className="flex flex-col">
-                    <h3 className="text-xs text-secondary">
-                      {user.company}
-                    </h3>
-                  </div>
-                </td>
-                <td className="text-sm font-medium text-gray-500 w-[12%]">
-                  <span className="text-xs text-secondary">
-                    {user.role}
-                  </span>
-                </td>
-                <td className="text-sm font-medium text-gray-500 w-[5%] flex items-center gap-2">
-                  <button className="outline-none border-none" type="button"><LuMoreVertical size={20}/></button>
+                <td className="text-sm font-medium text-gray-500 w-[10%] flex items-center gap-2">
+                  <button className="outline-none border-none text-red-600 hover:bg-red-600/40 p-2 rounded-lg" type="button">
+                    <HiMiniTrash size={20} />
+                  </button>
                 </td>
               </tr>
             </div>
@@ -77,7 +67,5 @@ const TemplateTable = () => {
     </div>
   );
 };
-
-
 
 export default TemplateTable;
