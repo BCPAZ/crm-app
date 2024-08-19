1e8b353655f1aeb0d7ddca944ee2b57ae28b6469
farildev <memmedovfaril@gmail.com>
2024-08-19 12:33:24 +0400

    feat : Components added to Change password

--- src/views/App/ChangePassword.jsx
+++ src/views/App/ChangePassword.jsx
@@ -1,6 +1,16 @@
+import Input from "@/components/common/Input"
+
 const ChangePassword = () => {
   return (
-    <div>ChangePassword</div>
+    <section className="w-full h-full">
+      <div className="siteContainer">
+        <h1 className="text-2xl font-semibold">Change password</h1>
+        <div>
+          <Input type="password" label="New password" placeholder="Enter new password" />
+          <Input type="password" label="Confirm new password" placeholder="Confirm new password" />
+        </div>
+      </div>
+    </section>
   )
 }
 
