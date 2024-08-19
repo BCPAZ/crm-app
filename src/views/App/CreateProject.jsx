925905319185776e03e4bffc28a7ce814e781603
farildev <memmedovfaril@gmail.com>
2024-08-19 10:13:20 +0400

    feat : Create project page added to views

---
+++ src/views/App/CreateProject.jsx
@@ -0,0 +1,11 @@
+const CreateProject = () => {
+  return (
+    <section className="w-full h-full">
+      <div className="siteContainer">
+        <h1 className="text-2xl font-semibold">Create project</h1>
+      </div>
+    </section>
+  )
+}
+
+export default CreateProject
