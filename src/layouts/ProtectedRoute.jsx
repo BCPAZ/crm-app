35f7f1183140c48d41c30a344da741c2421d9d3f
farildev <memmedovfaril@gmail.com>
2024-08-20 13:45:02 +0400

    feat : Protected route created

---
+++ src/layouts/ProtectedRoute.jsx
@@ -0,0 +1,10 @@
+import { Outlet , Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+const ProtectedRoute = () => {
+  const {isAuthenticated} = useSelector((state) => state.auth.isAuthenticated) 
+  return (
+    isAuthenticated ? <Outlet /> : <Navigate />
+  )
+}
+
+export default ProtectedRoute
