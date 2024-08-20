af69b022803a951f9867bb9e4d9fcc92e3ec25e0
farildev <memmedovfaril@gmail.com>
2024-08-20 13:15:58 +0400

    feat : Toast duration added

--- src/hooks/useToast.jsx
+++ src/hooks/useToast.jsx
@@ -2,21 +2,22 @@ import toast from "react-hot-toast";
 
 const useToast = () => {
   const showToast = (message, type = "info") => {
+    const duration = 5000;
     switch (type) {
       case "success":
-        toast.success(message);
+        toast.success(message, {duration});
         break;
       case "error":
-        toast.error(message);
+        toast.error(message, {duration});
         break;
       case "info":
-        toast.info(message);
+        toast.info(message, {duration});
         break;
       case "warning":
-        toast(message, { icon: "⚠️" });
+        toast(message, { icon: "⚠️" }, {duration});
         break;
       default:
-        toast(message);
+        toast(message, {duration});
     }
   };
   return {showToast}
