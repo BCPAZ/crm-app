39229f789b0a1265185c68f9a8bd8f40e8349491
farildev <memmedovfaril@gmail.com>
2024-08-23 00:01:58 +0400

    feat : Send mail service's form data completed

--- src/data/services/mailService.js
+++ src/data/services/mailService.js
@@ -13,6 +13,27 @@ const mailService = api.injectEndpoints({
         url: `/mails/${id}`,
       }),
     }),
+    sendMail : builder.query({
+      query : (data) => {
+        const formData = new FormData();
+        formData.append('to', data.to);
+        formData.append('subject', data.subject);
+        formData.append('message', data.message);
+        formData.append('cc', data.cc[0]);
+        formData.append('bcc',data.bcc[0]);
+        formData.append('reply_id', data.reply_id);
+        
+        if(data.attachment[0]){
+          formData.append('attachment', data.attachment[0]);
+        }
+
+        return{
+          url : '/mails',
+          method : 'POST',
+          body : formData
+        }
+      }
+    }),
     toggleStarredStatus: builder.mutation({
       query: ({ id, is_starred }) => ({
         url: `/mails/${id}/is-starred`,
@@ -28,6 +49,24 @@ const mailService = api.injectEndpoints({
         body: { is_important },
       }),
     }),
+    forceDelete : builder.mutation({
+      query : (id) => ({
+        url : `/mails/${id}/force`,
+        method : 'DELETE'
+      })
+    }),
+    softDelete : builder.mutation({
+      query : (id) => ({
+        url : `/mails/${id}`,
+        method : 'DELETE'
+      })
+    }),
+    restoreMail : builder.mutation({
+      query : (id) => ({
+        url : `/mails/${id}/restore`,
+        method : 'PATCH'
+      })
+    })
   }),
 });
 
