aca02b0423c15aa03073ad0ef3a703a6640b142d
farildev <memmedovfaril@gmail.com>
2024-08-22 15:44:05 +0400

    fix : Modal size fixed

--- src/components/App/Security/JobTitleModal.jsx
+++ src/components/App/Security/JobTitleModal.jsx
@@ -36,7 +36,7 @@ const JobTitleModal = ({ showModal, closeModal }) => {
 
   return (
     <div className={`w-full h-screen bg-black/70 ${showModal ? 'flex' : 'hidden'} items-center justify-center z-20 fixed top-0 left-0 right-0 bottom-0`}>
-      <div className="bg-white p-6 rounded-xl shadow-lg flex flex-col gap-5 w-1/2">
+      <div className="bg-white p-6 rounded-xl shadow-lg flex flex-col gap-5 min-w-1/2">
         <h1 className="text-xl font-semibold">Create new position</h1>
         <Alert type='primary' value="Şirkətdə istifadə edilmək üzərə pozisiya adı daxil edin" />
         <Input onChange={handlePositionName} value={positionName} label="Position name" type="text" placeholder="Add position name" />
