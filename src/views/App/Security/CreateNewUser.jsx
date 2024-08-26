a0b5d5ff2f9ec3cdcbb6b6713493b34b0815f4a9
Faril Mammadov <memmedovfaril@gmail.com>
2024-08-26 14:00:07 +0400

    chore : Browser image compression added

--- src/views/App/Security/CreateNewUser.jsx
+++ src/views/App/Security/CreateNewUser.jsx
@@ -83,7 +83,7 @@ const CreateNewUser = () => {
         <h1 className="text-2xl font-semibold">İstifadəçi yaradın</h1>
         <div className="w-full flex lg:flex-row flex-col justify-between gap-10 mt-24">
           <div className="lg:w-[38%] w-full flex flex-col bg-white rounded-xl shadow-lg p-6">
-            <div className="flex items-center flex-col">
+            <div className="flex items-center justify-center h-full flex-col">
               <div className="w-40 h-40 rounded-full overflow-hidden border-2 border-gray-500/50 relative group">
                 <img
                   className="w-full h-full object-cover"
