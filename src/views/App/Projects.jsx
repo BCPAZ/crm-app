2ec836ecbbb37a18dba3afca1bdfc59477b23d32
Faril Mammadov <memmedovfaril@gmail.com>
2024-08-28 12:50:40 +0400

    fix : Bugs fixed

--- src/views/App/Projects.jsx
+++ src/views/App/Projects.jsx
@@ -1,10 +1,13 @@
-import tasks from "@/mocks/tasks";
 import { Link } from "react-router-dom";
 import { FaUsers } from "react-icons/fa";
 import { LuMoreVertical, LuEye } from "react-icons/lu";
 import { IoAddSharp } from "react-icons/io5";
 import moment from "moment";
+import { useGetProjectsQuery } from "@/data/services/projectService";
+import Spinner from "@/components/common/Spinner";
 const Projects = () => {
+  const {data : projects = [], isLoading, isError} = useGetProjectsQuery();
+
   return (
     <section>
       <div className="siteContainer">
@@ -18,8 +21,10 @@ const Projects = () => {
             Proyekt yarat
           </Link>
         </div>
-        <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-3 mt-10">
-          {tasks.map((task, index) => (
+        <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-3 py-10">
+          {isError && <div className="w-full h-full flex items-center justify-center text-2xl font-semibold">Heç bir proyekt tapılmadı</div>}
+          {isLoading && <div className="flex items-center justify-center h-full"><Spinner /></div>}
+          {projects.map((project, index) => (
             <div
               className="relative p-6 rounded-lg border border-grey/20 flex flex-col gap-2"
               key={index}
@@ -31,23 +36,23 @@ const Projects = () => {
                 <LuMoreVertical size={20} />
               </button>
               <div className="flex flex-col gap-1 py-5 border-b border-grey/20">
-                <h1 className="text-xl font-semibold">{task.project}</h1>
+                <h1 className="text-xl font-semibold">{project.name}</h1>
                 <span className="text-xs font-base text-gray-400">
-                  Posted at : {moment(task.created_at).fromNow()}
+                  Yaradılma tarixi : {moment(project.created_at).fromNow()}
                 </span>
-                <p className="line-clamp-2 text-sm mt-3">{task.projectDesc}</p>
+                <p className="line-clamp-2 text-sm mt-3">{project.code}</p>
               </div>
               <div className="flex items-center justify-between gap-2 mt-3">
                 <Link
-                  className="text-white bg-secondary font-medium rounded-lg py-3 px-4 text-sm w-fit flex items-center gap-2"
-                  to={`${task.id}`}
+                  className="text-white bg-secondary font-medium rounded-lg py-2 px-4 text-sm w-fit flex items-center gap-2"
+                  to={`${project.id}`}
                 >
-                  <LuEye size={18} />
-                  See more
+                  <LuEye size={16} />
+                  Baxın
                 </Link>
                 <span className="flex items-center gap-2 text-xs font-semibold">
                   <FaUsers color="#00A76F" size={16} />
-                  {task.members} members
+                  {/* {project.members} members */}
                 </span>
               </div>
             </div>
