2b26f7ec1b4c92805959a33693b1dc36edbbb8a7
Faril Mammadov <memmedovfaril@gmail.com>
2024-08-29 12:37:12 +0400

    feat : resolve issue service completed

--- src/data/services/fieldService.js
+++ src/data/services/fieldService.js
@@ -9,31 +9,43 @@ const fieldService = api.injectEndpoints({
       providesTags: ["FIELDS"],
       keepUnusedDataFor: 0,
     }),
-    createIssue : builder.mutation({
-      query : (data) => {
+    createIssue: builder.mutation({
+      query: (data) => {
         const formData = new FormData();
-        formData.append('name', data.name);
-        formData.append('description', data.description);
-        formData.append('assignee_id', data.assignee_id);
-    
+        formData.append("name", data.name);
+        formData.append("description", data.description);
+        formData.append("assignee_id", data.assignee_id);
+
         if (data.files && data.files.length > 0) {
           data.files.forEach((file, index) => {
             formData.append(`files[${index}]`, file);
           });
         }
-    
+
         return {
-          url: '/field-management',
-          method: 'POST',
-          body: formData
+          url: "/field-management",
+          method: "POST",
+          body: formData,
         };
       },
-      invalidatesTags: ['FIELDS'],
-      keepUnusedDataFor: 0
+      invalidatesTags: ["FIELDS"],
+      keepUnusedDataFor: 0,
+    }),
+    resolveIssue: builder.mutation({
+      query: (id) => ({
+        url: `/field-management/${id}`,
+        method: "PATCH",
+      }),
+      invalidatesTags: ["FIELDS"],
+      keepUnusedDataFor: 0,
     }),
   }),
 });
 
-export const { useCreateIssueMutation, useGetAllIssuesQuery } = fieldService;
+export const {
+  useCreateIssueMutation,
+  useGetAllIssuesQuery,
+  useResolveIssueMutation,
+} = fieldService;
 
 export default fieldService;
