78c965e69b7a62bc35fe39db234c110c1becb7c8
Faril Mammadov <memmedovfaril@gmail.com>
2024-08-30 18:06:18 +0400

    fix : template service fixced

--- src/data/services/templateService.js
+++ src/data/services/templateService.js
@@ -1,41 +1,46 @@
 import api from "@/data/api";
 
 const templateService = api.injectEndpoints({
-  endpoints : (builder) => ({
-    getTemplates : builder.query({
-      query : () => ({
-        url : '/templates',
+  endpoints: (builder) => ({
+    getTemplates: builder.query({
+      query: () => ({
+        url: "/templates",
       }),
-      providesTags : ['TEMPLATES'],
-      keepUnusedDataFor : 0
+      providesTags: ["TEMPLATES"],
+      keepUnusedDataFor: 0,
     }),
-    createTemplate : builder.mutation({
-      query : (data) => ({
-        url : '/templates',
-        method : 'POST',
-        body : data
+    createTemplate: builder.mutation({
+      query: (data) => ({
+        url: "/templates",
+        method: "POST",
+        body: data,
       }),
-      invalidatesTags : ['TEMPLATES'],
-      keepUnusedDataFor : 0
+      invalidatesTags: ["TEMPLATES"],
+      keepUnusedDataFor: 0,
     }),
-    detailTemplate : builder.query({
-      query : (id) => ({
-        url : `templates/${id}`
+    detailTemplate: builder.query({
+      query: (id) => ({
+        url: `templates/${id}`,
       }),
-      providesTags : ['TEMPLATES'],
-      keepUnusedDataFor : 0
+      providesTags: ["TEMPLATES"],
+      keepUnusedDataFor: 0,
     }),
-    deleteTemplate : builder.mutation({
-      query : (id) => ({
-        url : `/templates/${id}`,
-        method : 'DELETE'
+    deleteTemplate: builder.mutation({
+      query: (id) => ({
+        url: `/templates/${id}`,
+        method: "DELETE",
       }),
-      invalidatesTags : ['TEMPLATES'],
-      keepUnusedDataFor : 0
-    })
-  })
-})
+      invalidatesTags: ["TEMPLATES"],
+      keepUnusedDataFor: 0,
+    }),
+  }),
+});
 
-export const {useCreateTemplateMutation, useGetTemplatesQuery} = templateService;
+export const {
+  useCreateTemplateMutation,
+  useGetTemplatesQuery,
+  useDeleteTemplateMutation,
+  useDetailTemplateQuery,
+} = templateService;
 
 export default templateService;
