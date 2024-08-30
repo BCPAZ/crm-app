c6519499f4b45e414f4a7c786cb4e5502a33e613
e1sndev <dev.e1sn@proton.me>
2024-08-30 20:03:59 +0400

    fix: CustomDatePicker fixed

--- src/components/common/CustomDatePicker.jsx
+++ src/components/common/CustomDatePicker.jsx
@@ -1,20 +1,17 @@
-import { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "@/assets/styles/datepicker.css";
 import { BsCalendar2DateFill } from "react-icons/bs";
 
 
-const CustomDatePicker = ({label}) => {
-  const [selectedDate, setSelectedDate] = useState(null);
-
+const CustomDatePicker = ({label, onChange, value}) => {
   return (
     <div className="relative">
       <label htmlFor="" className="text-sm">{label}</label>
       <div className="flex items-center justify-between border border-grey/20 p-3 rounded-lg">
       <DatePicker
-        selected={selectedDate}
-        onChange={(date) => setSelectedDate(date)}
+        selected={value}
+        onChange={onChange}
         className="w-full rounded-lg h-full bg-transparent text-sm text-gray-500"
         dateFormat="yyyy/MM/dd"
         placeholderText="YYYY/MM/DD"
