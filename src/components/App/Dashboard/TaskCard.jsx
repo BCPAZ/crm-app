2a460ebea732df2cc6b558235fde2d77c1642eaf
Faril Mammadov <memmedovfaril@gmail.com>
2024-11-13 14:46:40 +0400

    chore : priority names translated to azerbaijani

--- src/components/App/Dashboard/TaskCard.jsx
+++ src/components/App/Dashboard/TaskCard.jsx
@@ -1,19 +1,33 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import { FaRegCommentDots } from "react-icons/fa";
 import { IoIosAttach } from "react-icons/io";
 
 const TaskCard = ({ task }) => {
+  const priorityChecker = (priority) => {
+    switch (priority) {
+      case "HIGH":
+        return "Yüksək";
+      case "MEDIUM":
+        return "Orta";
+      case "LOW":
+        return "Aşağı";
+    }
+  };
   return (
     <div className="flex flex-col">
       <div className="flex items-start justify-between gap-3 py-[22px] border-b-2 border-grey/20">
         <div className="flex flex-col gap-4 w-full">
           <div className="flex justify-between items-center gap-2 w-full">
             <span className="text-md text-black font-medium">{task.name}</span>
-            <span className="text-xs text-gray-400">{task.priority}</span>
+            <span className="text-xs text-gray-400">{priorityChecker(task.priority)}</span>
           </div>
-          <div className='flex items-center gap-3'>
-            <span className='flex items-center gap-2 text-sm font-medium text-gray-400'><FaRegCommentDots size={18}/> : {task.comments_count || 0}</span>
-            <span className='flex items-center gap-2 text-sm font-medium text-gray-400'><IoIosAttach size={18}/> : {task.attachments_count || 0}</span>
+          <div className="flex items-center gap-3">
+            <span className="flex items-center gap-2 text-sm font-medium text-gray-400">
+              <FaRegCommentDots size={18} /> : {task.comments_count || 0}
+            </span>
+            <span className="flex items-center gap-2 text-sm font-medium text-gray-400">
+              <IoIosAttach size={18} /> : {task.attachments_count || 0}
+            </span>
           </div>
         </div>
       </div>
@@ -25,8 +39,8 @@ TaskCard.propTypes = {
   task: PropTypes.shape({
     name: PropTypes.string.isRequired,
     priority: PropTypes.string.isRequired,
-    attachments_count : PropTypes.number,
-    comments_count : PropTypes.number
+    attachments_count: PropTypes.number,
+    comments_count: PropTypes.number,
   }).isRequired,
 };
 
