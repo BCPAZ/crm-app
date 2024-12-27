59b6f2fbee9a542f67ff15bd23954e8528cc344c
Faril Mammadov <memmedovfaril@gmail.com>
2024-12-28 00:41:32 +0400

    feat : Navigate link added

--- src/components/App/Dashboard/ProjectCard.jsx
+++ src/components/App/Dashboard/ProjectCard.jsx
@@ -1,15 +1,16 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 const ProjectCard = ({ project }) => {
   return (
     <div className="flex flex-col">
-      <div className="flex items-center justify-between gap-2 p-[22px] border-b-2 border-grey/20">
+      <Link to={`/projects/${project.id}`} className="flex items-center justify-between gap-2 p-[22px] border-b-2 border-grey/20">
         <h1 className="text-md text-black font-medium">{project.name}</h1>
         <div className="flex items-center gap-2">
           <span className="text-xs text-grey">
            #{project.code}
           </span>
         </div>
-      </div>
+      </Link>
       <div></div>
     </div>
   );
@@ -17,6 +18,7 @@ const ProjectCard = ({ project }) => {
 
 ProjectCard.propTypes = {
   project : PropTypes.shape({
+    id : PropTypes.string,
     code : PropTypes.string,
     name : PropTypes.string
   })
